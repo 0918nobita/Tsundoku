a33ebf9e155f61b8133b60dd605cd5f622e005e8
0918nobita <nobita.0918@gmail.com>
2018-07-29 14:34:22 +0900

    Todo[] 型データを export する todo-list.ts を追加した

---
+++ src/app/todo-list.ts
@@ -0,0 +1,16 @@
+import { Todo } from './todo';
+
+export const todoList: Todo[] = [
+  {
+    title: 'Get started Angular',
+    completed: false,
+  },
+  {
+    title: 'Write an article',
+    completed: false,
+  },
+  {
+    title: 'Get the Hikarie',
+    completed: true,
+  },
+];
