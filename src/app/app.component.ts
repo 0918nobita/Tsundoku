e074ff80866db53ead5a5c1ab78ba793962c1c9f
0918nobita <nobita.0918@gmail.com>
2018-07-29 14:50:40 +0900

    トップページでTodoリストを出力するようにした

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { todoList } from './todo-list';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Tsundoku.tech';
+  todoList = todoList;
 }
