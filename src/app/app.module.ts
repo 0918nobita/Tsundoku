ee7c95a85e73753d1a92cd91da1cb86f7efdb21e
0918nobita <nobita.0918@gmail.com>
2018-07-29 15:15:13 +0900

    双方向バインディングを用いて、Todoのcompletedプロパティをチェックボックスから操作できるようにした

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
