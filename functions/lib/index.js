410616cc3e833e51c58faa114e58be2aa7d31c0b
0918nobita <nobita.0918@gmail.com>
2018-07-30 21:19:19 +0900

    :white_check_mark: Add an example of Cloud Functions for Firebase

---
+++ functions/lib/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const functions = require("firebase-functions");
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
+});
+//# sourceMappingURL=index.js.map
