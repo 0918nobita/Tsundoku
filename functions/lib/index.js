ead0fedd343ee8f53c3556da70c2fc26047c7a59
0918nobita <nobita.0918@gmail.com>
2018-07-31 21:10:00 +0900

    :sparkles: Enable to get the information about the books registered into Cloud Firestore

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -1,6 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const admin = require("firebase-admin");
 const functions = require("firebase-functions");
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+exports.getBooks = functions.https.onCall((data, context) => {
+    return db.collection('books').get()
+        .then(result => {
+        let str = '';
+        result.forEach(doc => {
+            const docData = doc.data();
+            const id = (docData.asin !== void 0) ? docData.asin : docData.isbn13;
+            str += doc.id + ' => ' + docData.title + ', ' + docData.author + ', ' + id + '\n';
+        });
+        return str;
+    })
+        .catch(error => error);
+});
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 exports.helloWorld = functions.https.onRequest((request, response) => {
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,23 @@
+import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
+
+admin.initializeApp(functions.config().firebase);
+
+const db = admin.firestore();
+
+export const getBooks = functions.https.onCall((data, context) => {
+  return db.collection('books').get()
+    .then(result => {
+      let str = '';
+      result.forEach(doc => {
+        const docData = doc.data();
+        const id = (docData.asin !== void 0) ? docData.asin : docData.isbn13;
+        str += doc.id + ' => ' + docData.title + ', ' + docData.author + ', ' + id + '\n';
+      });
+      return str;
+    })
+    .catch(error => error);
+});
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
