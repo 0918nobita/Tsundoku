39d9bc6c3dfc2b80da3cde48985b5f489ec1af79
0918nobita <nobita.0918@gmail.com>
2018-08-01 01:42:15 +0900

    :sparkles: Enable to display thumbnail images of the books registered into Cloud Firestore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { Todo } from './todo';
+import { config } from './config';
 import { todoList } from './todo-list';
+import * as firebase from 'firebase/app';
+import 'firebase/functions';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +14,18 @@ import { todoList } from './todo-list';
 export class AppComponent {
   title = 'Tsundoku.tech';
   todoList = todoList;
+  books: any = [];
+
+  constructor() {
+    firebase.initializeApp(config);
+    
+    const getBooks = firebase.functions().httpsCallable('getBooks');
+    getBooks().then(result => {
+      this.books = result.data;
+    }).catch(
+      error => console.log(error.details)
+    );
+  }
 
   addTodo(todo: Todo) {
     this.todoList.unshift(todo);
