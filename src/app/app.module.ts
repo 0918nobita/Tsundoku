f243484da666b885cb3b2b3730098940c09c155f
0918nobita <nobita.0918@gmail.com>
2018-08-02 21:47:26 +0900

    🔧 Enable to use ServiceWorkerModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoFormComponent } from './todo-form/todo-form.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,7 @@ import { TodoFormComponent } from './todo-form/todo-form.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production}),
   ],
   providers: [],
   bootstrap: [AppComponent]
