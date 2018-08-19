a56cc232135b8546eb645b2bfcb0eb27bf6488c6
0918nobita <nobita.0918@gmail.com>
2018-08-20 01:39:58 +0900

    Resolvedインターフェースを追加した

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,14 @@
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 
+interface ResolvedBook {
+  desc: string;
+  donor: string;
+  image: string;
+  isbn: string;
+  title: string;
+}
+
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 db.settings({ timestampsInSnapshots: true });
