3c6531fd1499c856dc31d7168ef60b7882f26a10
0918nobita <nobita.0918@gmail.com>
2018-08-26 17:46:06 +0900

    :truck: appコンポーネントの処理をsearchコンポーネントに移植した

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,4 @@
-import axios from 'axios';
 import { Component } from '@angular/core';
-import { config } from './config';
-import * as firebase from 'firebase/app';
-import 'firebase/functions';
-
-declare var $;
-
-interface ResolvedBook {
-  desc: string;
-  donor: string;
-  image: string;
-  isbn: string;
-  title: string;
-}
 
 @Component({
   selector: 'app-root',
@@ -21,46 +7,5 @@ interface ResolvedBook {
 })
 
 export class AppComponent {
-  hitBooks: Array<ResolvedBook> = [];
-  content = '';
-  functions: firebase.functions.Functions;
-
-  constructor() {
-    firebase.initializeApp(config);
-
-    this.functions = firebase.functions();
-  }
-
-  search(isbn: string) {
-    if ((isbn.length !== 10) && (isbn.length !== 13)) {
-      $('#errorModal').modal();
-      return;
-    }
-
-    const searchBooksByISBN = (isbn: string): Promise<Array<ResolvedBook>> =>
-      this.functions.httpsCallable('searchBooksByISBN')(isbn)
-        .then(result => result.data)
-        .catch(error => error);
-
-    axios.get('https://www.googleapis.com/books/v1/volumes?q=isbn:' + isbn)
-      .then(async result => {
-        this.hitBooks = [];
-
-        if (result.data.items !== void 0) {
-          // ヒットした場合は取り出してサムネを出力する
-          result.data.items.map((item, index) =>
-            this.hitBooks.push({
-              desc: item.volumeInfo.description,
-              donor: 'none',
-              image: 'https' + item.volumeInfo.imageLinks.smallThumbnail.slice(4),
-              isbn: isbn,
-              title: item.volumeInfo.title
-            }));
-        } else {
-          // ヒットしなかった場合は resolvedBooks で検索する
-          this.hitBooks = await searchBooksByISBN(isbn);
-        }
-      })
-      .catch(error => 'Error: ' + error);
-  }
+  constructor() {}
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,15 +1,68 @@
+import axios from 'axios';
+import { config } from '../config';
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase/app';
+import 'firebase/functions';
+
+declare var $;
+
+interface ResolvedBook {
+  desc: string;
+  donor: string;
+  image: string;
+  isbn: string;
+  title: string;
+}
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
+
 export class SearchComponent implements OnInit {
+  ngOnInit() {}
 
-  constructor() { }
+  hitBooks: Array<ResolvedBook> = [];
+  content = '';
+  functions: firebase.functions.Functions;
 
-  ngOnInit() {
+  constructor() {
+    firebase.initializeApp(config);
+
+    this.functions = firebase.functions();
   }
 
+  search(isbn: string) {
+    if ((isbn.length !== 10) && (isbn.length !== 13)) {
+      $('#errorModal').modal();
+      return;
+    }
+
+    const searchBooksByISBN = (isbn: string): Promise<Array<ResolvedBook>> =>
+      this.functions.httpsCallable('searchBooksByISBN')(isbn)
+        .then(result => result.data)
+        .catch(error => error);
+
+    axios.get('https://www.googleapis.com/books/v1/volumes?q=isbn:' + isbn)
+      .then(async result => {
+        this.hitBooks = [];
+
+        if (result.data.items !== void 0) {
+          // ヒットした場合は取り出してサムネを出力する
+          result.data.items.map((item, index) =>
+            this.hitBooks.push({
+              desc: item.volumeInfo.description,
+              donor: 'none',
+              image: 'https' + item.volumeInfo.imageLinks.smallThumbnail.slice(4),
+              isbn: isbn,
+              title: item.volumeInfo.title
+            }));
+        } else {
+          // ヒットしなかった場合は resolvedBooks で検索する
+          this.hitBooks = await searchBooksByISBN(isbn);
+        }
+      })
+      .catch(error => 'Error: ' + error);
+  }
 }
