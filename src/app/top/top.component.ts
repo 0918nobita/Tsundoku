a2b91e04cb1562a61fb236a8924982ea09a5f6cf
0918nobita <nobita.0918@gmail.com>
2018-09-02 19:21:10 +0900

    :memo: TopComponent クラスのコントラクタとrouterプロパティの ESDoc を追加した

--- src/app/top/top.component.ts
+++ src/app/top/top.component.ts
@@ -12,7 +12,20 @@ declare var $;  // jQuery
   styleUrls: ['./top.component.css']
 })
 export class TopComponent implements OnInit {
-  constructor(private router: Router) {}
+  private router: Router;
+
+  /**
+   * コンポーネントの初期化
+   * @param {Router} router - Angular の Router
+   */
+  constructor(router: Router) {
+    /**
+     * Angular の Router
+     * @private
+     * @type {Router} router
+     */
+    this.router = router;
+  }
 
   ngOnInit() {}
 }
