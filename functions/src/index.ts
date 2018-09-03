795483a3178e73cf719eef85b6e2ed929ccbf7eb
0918nobita <nobita.0918@gmail.com>
2018-09-03 21:15:31 +0900

    :heavy_plus_sign: Functions側にfirebaseパッケージを導入した

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,6 @@
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
+import * as firebase from 'firebase';
 import { ehb } from './ehb';
 
 interface ResolvedBook {
