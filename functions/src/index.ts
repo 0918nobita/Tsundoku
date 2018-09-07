b6b98c848bc57de1683bd6139129d380ba0783f2
0918nobita <nobita.0918@gmail.com>
2018-09-07 20:42:13 +0900

    :truck: Functions側のそれぞれの関数をlocalFunctions名前空間が内包するようにした

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,46 +9,56 @@ admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 db.settings({ timestampsInSnapshots: true });
 
-// resolvedBooks コレクションで本を検索する
-export const searchBooksByISBN = functions.https.onCall((isbn: string) =>
-  db.collection('resolvedBooks')
-    .where('isbn', '==', isbn)
-    .get()
-    .then(querySnapshot => {
-      const response: Array<ResolvedBook> = [];
-      querySnapshot.forEach(doc => {
-        const docData = doc.data();
-        response.push({
-          desc: docData.desc,
-          donor: docData.donor,
-          image: docData.image,
-          isbn: docData.isbn,
-          title: docData.title
+namespace localFunctions {
+  // resolvedBooks コレクションで本を検索する
+  export const searchBooksByISBN = (isbn: string): Promise<Array<ResolvedBook>> =>
+    db.collection('resolvedBooks')
+      .where('isbn', '==', isbn)
+      .get()
+      .then(querySnapshot => {
+        const response: Array<ResolvedBook> = [];
+        querySnapshot.forEach(doc => {
+          const docData = doc.data();
+          response.push({
+            desc: docData.desc,
+            donor: docData.donor,
+            image: docData.image,
+            isbn: docData.isbn,
+            title: docData.title
+          });
         });
-      });
-      return response;
-    })
-    .catch(error => error)
-);
+        return response;
+      })
+      .catch(error => error);
 
-export const getBookshelf = functions.https.onCall((user: string) =>
-  db.collection('bookshelf')
-    .where('user', '==', user)
-    .get()
-    .then(querySnapshot => {
-      const response: Array<RegisteredBook> = [];
-      querySnapshot.forEach(doc => {
-        const docData = doc.data();
-        response.push({
-          deadline: docData.deadline,
-          favorite: docData.favorite,
-          isbn: docData.isbn,
-          progress: Progress.parse(docData.progress)
-        });
-      });
-      return response;
-    })
-    .catch(error => error)
-);
+  export const getBookshelf = (user: string): Promise<Array<RegisteredBook>> =>
+    db.collection('bookshelf')
+      .where('user', '==', user)
+      .get()
+      .then(async querySnapshot => {
+        const response: Array<RegisteredBook> = [];
+
+        for (let i = 0; i < querySnapshot.size; i++) {
+          const docData = querySnapshot[i].data();
+
+          // ISBNを元に本の詳細情報を取得する
+          const book = await localFunctions.searchBooksByISBN(docData.isbn);
+          if (book.length !== 0) {
+            response.push({
+              deadline: docData.deadline,
+              favorite: docData.favorite,
+              isbn: docData.isbn,
+              progress: Progress.parse(docData.progress)
+            });
+          }
+        }
+        return response;
+      })
+      .catch(error => error)
+}
+
+export const searchBooksByISBN = functions.https.onCall(localFunctions.searchBooksByISBN);
+
+export const getBookshelf = functions.https.onCall(localFunctions.getBookshelf);
 
 export const getContributions = functions.https.onRequest(ehb);
