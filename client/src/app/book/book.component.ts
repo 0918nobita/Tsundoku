a73603ffb551eae8bb2fb0fa961db333583f728d
0918nobita <nobita.0918@gmail.com>
2018-09-09 20:21:32 +0900

    :sparkles: 本棚画面で、お気に入り追加している本のサムネイルの上に星マークが重ねて表示されるようにした

--- client/src/app/book/book.component.ts
+++ client/src/app/book/book.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
+import * as $ from 'jquery';
 
 /**
  * 本棚画面や検索画面で表示される、<br>
@@ -10,18 +11,25 @@ import { Router } from '@angular/router';
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.css']
 })
-export class BookComponent implements OnInit {
+export class BookComponent implements OnInit, AfterViewInit {
   @Input() public title: string;
   @Input() public author: string;
   @Input() public desc: string;
   @Input() public isbn: string;
   @Input() public image: string;
+  @Input() public favorite: boolean;
 
   private visible = false;
 
   constructor(private router: Router) {}
 
   ngOnInit() {}
+
+  ngAfterViewInit() {
+    if (this.favorite === true) {
+      $('#star' + this.isbn).css('display': 'block');
+    }
+  }
 
   /** 本の詳細ページに遷移する */
   show(isbn: string) {
