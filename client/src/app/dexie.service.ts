b92fd04af5eb84593cc46a907037b6c0cf06555d
0918nobita <nobita.0918@gmail.com>
2018-09-12 18:38:36 +0900

    :wrench: DexieServiceに、bookshelfオブジェクトストアについての設定を追加した

--- client/src/app/dexie.service.ts
+++ client/src/app/dexie.service.ts
@@ -8,7 +8,8 @@ export class DexieService extends Dexie {
   constructor() {
     super('local-database');
     this.version(1).stores({
-      resolvedBooks: '++id, desc, donor, image, &isbn, title, pageCount'
+      resolvedBooks: '++id, desc, donor, image, &isbn, title, pageCount',
+      bookshelf: '++id, deadline, favorite, progress, [isbn + user]'
     });
   }
 }
