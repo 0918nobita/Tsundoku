be447bd96c011b34ed62d92a956800ad0401b638
0918nobita <nobita.0918@gmail.com>
2018-09-12 17:59:48 +0900

    :wrench: resolvedBooksオブジェクトストアについての設定をDexieServiceに追加した

--- client/src/app/dexie.service.ts
+++ client/src/app/dexie.service.ts
@@ -8,7 +8,7 @@ export class DexieService extends Dexie {
   constructor() {
     super('local-database');
     this.version(1).stores({
-      resolvedBooks: '++, isbn'
+      resolvedBooks: '++id, desc, donor, image, &isbn, title, pageCount'
     });
   }
 }
