73722f4e3d34495397f30931a50feb73cd5ed16d
0918nobita <nobita.0918@gmail.com>
2018-09-14 16:47:14 +0900

    :construction: AccountService の myself プロパティでユーザーデータを保持するようにした

--- client/src/app/account.service.ts
+++ client/src/app/account.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { User } from 'shared/entity';
 
 /** 登録 / ログイン / ログアウト / 退会処理, ログイン中のアカウントの情報の保持 を担当する */
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
+  private myself: User;
+
   constructor() {}
 }
