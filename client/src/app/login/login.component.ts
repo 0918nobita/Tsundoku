7c8e952a672f7e87dd032bb9fd9adde7adfb43bf
0918nobita <nobita.0918@gmail.com>
2018-09-15 03:09:00 +0900

    :construction: LoginComponent で、AccountService を DI するようにした

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../account.service';
 
 /**
  * ログイン画面
@@ -9,6 +10,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  constructor(private accountService: AccountService) {}
+
   ngOnInit() {}
 }
