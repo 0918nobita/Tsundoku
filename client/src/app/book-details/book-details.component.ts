898fb3f5ab68945ab860da0ec155be13f140d5db
0918nobita <nobita.0918@gmail.com>
2018-09-15 06:37:26 +0900

    :construction: book-details.component.tsでの、サービスのインポート元のパスを修正した

--- client/src/app/book-details/book-details.component.ts
+++ client/src/app/book-details/book-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { BookService } from '../book.service';
+import { BookService } from '../services/book.service';
 import { ActivatedRoute } from '@angular/router';
 import { ResolvedBook } from 'shared/entity';
 
