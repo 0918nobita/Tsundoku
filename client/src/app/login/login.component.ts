7408a712c6d3fc8c4458c9c71cb143eef700d525
0918nobita <nobita.0918@gmail.com>
2018-09-15 06:39:12 +0900

    :construction: login.component.tsでの、サービスのインポート元のパスを修正した

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AccountService } from '../account.service';
+import { AccountService } from '../services/account.service';
 
 /**
  * ログイン画面
