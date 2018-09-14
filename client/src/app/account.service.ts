02954e14069cf193c62854cc68bbf018684bf23b
0918nobita <nobita.0918@gmail.com>
2018-09-14 19:03:02 +0900

    :construction: AccountService で、FirebaseService を DI するようにした

--- client/src/app/account.service.ts
+++ client/src/app/account.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { User } from 'shared/entity';
+import { FirebaseService } from './firebase.service';
 
 /** 登録 / ログイン / ログアウト / 退会処理, ログイン中のアカウントの情報の保持 を担当する */
 @Injectable({
@@ -7,8 +8,11 @@ import { User } from 'shared/entity';
 })
 export class AccountService {
   private myself: User;
+  private auth: firebase.auth.Auth;
 
-  constructor() {}
+  constructor(private firebaseService: FirebaseService) {
+    this.auth = this.firebaseService.auth;
+  }
 
   get uid() { return this.myself && this.myself.uid; }
 
