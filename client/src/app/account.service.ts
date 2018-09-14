94e94ff54b15a614bbe1069d3276df1762b225b7
0918nobita <nobita.0918@gmail.com>
2018-09-14 16:46:29 +0900

    :art: Improve readability

--- client/src/app/account.service.ts
+++ client/src/app/account.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-/** 登録処理, ログイン処理, ログイン中のアカウントの情報の保持, ログアウト, 退会処理 を担当する */
+/** 登録 / ログイン / ログアウト / 退会処理, ログイン中のアカウントの情報の保持 を担当する */
 @Injectable({
   providedIn: 'root'
 })
