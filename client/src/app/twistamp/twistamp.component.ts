0908df0d3e29c5d43d45a2ca79860b488124885e
0918nobita <nobita.0918@gmail.com>
2018-09-15 13:52:14 +0900

    :sparkles: TwiStampのログイン機能のMVPを作成した

--- client/src/app/twistamp/twistamp.component.ts
+++ client/src/app/twistamp/twistamp.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, AfterViewInit, ElementRef } from '@angular/core';
+import { FirebaseService } from '../services/firebase.service';
+import * as $ from 'jquery';
+/*import * as firebase from 'firebase/app';
+import "firebase/auth"*/
 
 @Component({
   selector: 'app-twistamp',
@@ -6,12 +10,25 @@ import { Component, OnInit, AfterViewInit, ElementRef } from '@angular/core';
   styleUrls: ['./twistamp.component.css']
 })
 export class TwistampComponent implements OnInit, AfterViewInit {
-  constructor(private elementRef: ElementRef) {}
+  functions: firebase.functions.Functions;
+
+  constructor(private elementRef: ElementRef,
+              private firebaseService: FirebaseService) {}
 
   ngOnInit() {}
 
   ngAfterViewInit() {
     this.elementRef.nativeElement.ownerDocument.body.style.color = 'black';
     this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = '#E2E9EE';
+  }
+
+  login() {
+    /*this.firebaseService.auth.signInWithPopup(new firebase.auth.TwitterAuthProvider())
+      .then(result => {
+        console.log(result.user);
+      })
+      .catch(error => console.log(error));*/
+    $('#login').hide();
+    $('#myself').show();
   }
 }
