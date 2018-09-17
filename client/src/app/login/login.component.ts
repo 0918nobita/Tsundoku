1c5d501a496a6ab5f192dd757598910c65b81dad
0918nobita <nobita.0918@gmail.com>
2018-09-17 21:38:42 +0900

    :sparkles: ログイン画面を表示すると、Firebase Authentication でオートログインを行い、成功すれば本棚画面に遷移するようにした

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { AccountService } from '../services/account.service';
 
 /**
@@ -10,12 +12,24 @@ import { AccountService } from '../services/account.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  constructor(private accountService: AccountService) {}
+  constructor(private router: Router,
+              private accountService: AccountService) {
+    this.accountService.auth.onAuthStateChanged(user => {
+      if (user) {
+        this.router.navigate(['/bookshelf']);
+      } else {
+      }
+    });
+  }
 
   ngOnInit() {}
 
   async login(email, password) {
-    /*const result = await this.accountService.login(email, password);
-    if (result === true) console.log(this.accountService.bio);*/
+    try {
+      const result = await this.accountService.login(email, password);
+      this.router.navigate(['/bookshelf']);
+    } catch(error) {
+      console.log(error);
+    }
   }
 }
