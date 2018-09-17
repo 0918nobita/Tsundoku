82bf7504d18fde44de5f5facc1c95af0e94beb23
0918nobita <nobita.0918@gmail.com>
2018-09-18 07:24:45 +0900

    :art: Improve readability

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent implements AfterViewInit {
   constructor(private router: Router,
               private elementRef: ElementRef,
               private accountService: AccountService) {
-    this.accountService.afterLogin(user => {
+    this.accountService.afterLogin(() => {
       if (['/', '/login', '/register'].indexOf(location.pathname) !== -1)
         this.router.navigate(['/bookshelf']);
       $('#routerOutlet').show();
