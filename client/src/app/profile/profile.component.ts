7a4d6367f08149cffab6cd4bf3151f3bd3e12084
0918nobita <nobita.0918@gmail.com>
2018-09-18 03:17:06 +0900

    :sparkles: プロフィール画面で、実際に指定したnameのユーザーの情報をFunctions経由で取得し、表示するようにした

--- client/src/app/profile/profile.component.ts
+++ client/src/app/profile/profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { AccountService } from '../services/account.service';
+
+import { User } from 'shared/entity';
+import { FirebaseService } from '../services/firebase.service';
 
 /**
  * プロフィール画面
@@ -13,16 +15,36 @@ import { AccountService } from '../services/account.service';
 export class ProfileComponent implements OnInit {
   name: string;
   screenName: string;
+  bio: string;
   image: string;
 
   constructor(private activatedRoute: ActivatedRoute,
-              private accountService: AccountService) {}
+              private firebaseService: FirebaseService) {}
 
-  ngOnInit() {
+  async ngOnInit() {
     this.name = this.activatedRoute.snapshot.params['name'];
-    if (this.accountService.currentUser === null) return;
-    this.image = this.accountService.image;
-    this.screenName = this.accountService.screenName;
-    console.log([this.image, this.screenName]);
+
+    const getUserByName = (name: string): Promise<User> =>
+      this.firebaseService.functions.httpsCallable('getUsersByName')(name)
+        .then(result => {
+          if (result.data.length > 0) {
+            return <User> result.data[0];
+          } else {
+            return null;
+          }
+        })
+        .catch(error => {
+          console.log(error);
+          return null;
+        });
+
+    try {
+      const user = await getUserByName(this.name);
+      this.screenName = user.screenName;
+      this.bio = user.bio;
+      this.image = user.image;
+    } catch(error) {
+      console.log(error);
+    }
   }
 }
