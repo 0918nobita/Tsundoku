c3cd1a0a75ff1c1aebe06e7751d17a50b8098c7d
0918nobita <nobita.0918@gmail.com>
2018-09-19 08:16:45 +0900

    :sparkles: UserService に getUserByName メソッドを追加した

--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
 
+import { User } from 'shared/entity';
+import { FirebaseService } from './firebase.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  constructor() {}
+  constructor(private firebaseService: FirebaseService) {}
+
+  async getUserByName(name: string) {
+    try {
+      const response =
+          await this.firebaseService.functions.httpsCallable('getUserByName')(name);
+      return (response.data.length > 0) ? <User> response.data[0] : null;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
 }
