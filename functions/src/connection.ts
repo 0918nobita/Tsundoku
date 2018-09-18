41413a487490651af1dbad23d5482d8f4d077e5e
0918nobita <nobita.0918@gmail.com>
2018-09-18 18:33:36 +0900

    :sparkles: Functions側に、getUsersConnectedFrom 関数を追加した

--- functions/src/connection.ts
+++ functions/src/connection.ts
@@ -1,3 +1,6 @@
+import { User } from '../../shared/entity';
+import { _getUsersBy } from './user';
+
 export const _checkConnectionFrom = (db: FirebaseFirestore.Firestore) =>
   (args: {name: string, to: string}) =>
     db.collection('connections')
@@ -6,3 +9,28 @@ export const _checkConnectionFrom = (db: FirebaseFirestore.Firestore) =>
       .get()
       .then(querySnapshot => querySnapshot.size > 0);
 
+export const _getUsersConnectedFrom = (db: FirebaseFirestore.Firestore) =>
+  (name: string) => new Promise(async (resolve: (value: User[]) => void,
+                                       reject:  (reason?: any)  => void) => {
+    try {
+      const users: User[] = [];
+      const names = await db.collection('connections')
+        .where('from', '==', name)
+        .get()
+        .then(querySnapshot => {
+          const response: string[] = [];
+
+          for (let i = 0; i < querySnapshot.size; i++)
+            response.push(querySnapshot.docs[i].data().to);
+
+          return response;
+        });
+
+      for (let i = 0; i < names.length; i++)
+        users.concat(await _getUsersBy('name', db)(names[i]));
+
+      resolve(users);
+    } catch(error) {
+      reject(error);
+    }
+  });
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,7 @@ import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 
 import { _getUsersBy } from './user';
-import { _checkConnectionFrom } from './connection';
+import { _checkConnectionFrom, _getUsersConnectedFrom } from './connection';
 import { _searchBooksByISBN, _postResolvedBook } from './book';
 import { _getBookshelf } from './bookshelf';
 
@@ -14,6 +14,7 @@ export const getUsersByUID = functions.https.onCall(_getUsersBy('uid', db));
 export const getUsersByName = functions.https.onCall(_getUsersBy('name', db));
 
 export const checkConnectionFrom = functions.https.onCall(_checkConnectionFrom(db));
+export const getUsersConnectedFrom = functions.https.onCall(_getUsersConnectedFrom(db));
 
 export const searchBooksByISBN = functions.https.onCall(_searchBooksByISBN(db));
 export const postResolvedBook = functions.https.onCall(_postResolvedBook(db));
