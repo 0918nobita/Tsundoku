6de4d27c2eb831b0ddc9f95cd76f84ad3129a5ee
0918nobita <nobita.0918@gmail.com>
2018-09-19 18:25:41 +0900

    :art: app-progress-card 要素のInput属性を record のみにした
    ユーザー情報の取得は
     ProgressCardComponent 側で行う

--- client/src/app/book-details/book-details.component.ts
+++ client/src/app/book-details/book-details.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { Progress } from 'shared/progress';
+import { Record } from 'shared/entity';
 import { BookService } from '../services/book.service';
 import { FirebaseService } from '../services/firebase.service';
-import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-book-details',
@@ -18,26 +17,19 @@ export class BookDetailsComponent implements OnInit {
   isbn: string;
   image: string;
   pageCount: string;
-  records: any[];
+  records: Record[];
 
   constructor(private activatedRoute: ActivatedRoute,
               private bookService: BookService,
-              private firebaseService: FirebaseService,
-              private userService: UserService) {}
+              private firebaseService: FirebaseService) {}
 
   async ngOnInit() {
-
-    const convertDateTime = timestamp => {
-      const pad = input => (input < 10) ? '0' + input : input;
-      const date = timestamp ? new Date(timestamp * 1000) : new Date();
-      return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()} ` +
-          `${pad(date.getHours())}:${pad(date.getMinutes())}:${pad(date.getSeconds())}`;
-    };
 
     const isbn: string = this.activatedRoute.snapshot.params['isbn'];
 
     try {
       const book = await this.bookService.getBookByISBN(isbn);
+
       $('app-now-loading').hide();
       if (book === null) return;
       this.title = book.title;
@@ -48,15 +40,6 @@ export class BookDetailsComponent implements OnInit {
       this.pageCount = `ページ数: ${ book.pageCount }`;
 
       this.records = await this.getRecordsByISBN(book.isbn);
-      for (let i = 0; i < this.records.length; i++) {
-        this.records[i].range =
-          (new Progress(this.records[i].range.fragments)).toString()
-            .replace(/-/g, '〜') + 'ページ';
-        this.records[i].created = convertDateTime(this.records[i].created._seconds);
-        const user = await this.userService.getUserByName(this.records[i].user);
-        this.records[i].screenName = user.screenName;
-        this.records[i].image = user.image;
-      }
     } catch (error) {
       console.error(error);
     }
