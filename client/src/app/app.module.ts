977056959d4525c8fe01e20e0b3722096a6c7086
0918nobita <nobita.0918@gmail.com>
2018-09-19 18:34:25 +0900

    :art: app.module.ts から必要ない import 文を除去した

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { RouterModule } from '@angular/router';
-import * as bootstrap from 'bootstrap';
 
 import { environment } from '../environments/environment';
 
