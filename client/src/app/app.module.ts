6c3e3231b6c760a773b63496e4bba429f091b655
0918nobita <nobita.0918@gmail.com>
2018-09-20 17:54:53 +0900

    :pencil2: Fix typo

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { RouterModule } from '@angular/router';
-require('bootstrap');
+import 'bootstrap';
 
 import { environment } from '../environments/environment';
 
