a84e96aec819b859ad720c2ea1c406ea0c6f7e4e
0918nobita <nobita.0918@gmail.com>
2018-09-29 07:10:11 +0900

    :art: 必要ない import 文を除去した

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import * as $ from 'jquery';
 
 import { AccountService } from '../services/account.service';
 
