65b82e2cdb51b0092435e573c47606cc03cd1b6a
0918nobita <nobita.0918@gmail.com>
2018-09-28 20:10:38 +0900

    :construction: Observable を用いて、ネットワーク接続状態の変更を検知するサンプルを追加した

--- client/src/app/services/firebase.service.ts
+++ client/src/app/services/firebase.service.ts
@@ -4,6 +4,7 @@ import 'firebase/functions';
 import 'firebase/auth';
 
 import * as config from '../config.json';
+import { fromEvent, merge } from 'rxjs';
 
 /**
  * Firebase SDK の設定を各コンポーネントで共有するための、Angular の Service
@@ -20,5 +21,13 @@ export class FirebaseService {
     firebase.initializeApp(config);
     this.auth = firebase.auth();
     this.functions = firebase.functions();
+
+    const observable = merge(
+        fromEvent(window, 'online'),
+        fromEvent(window, 'offline'));
+
+    observable.subscribe((event) => {
+      console.log('接続状態が変化しました', event);
+    });
   }
 }
