c2270ce10eea57e6c871f6418844d7b94076bb02
0918nobita <nobita.0918@gmail.com>
2018-09-30 21:15:56 +0900

    :construction: RxJS のサンプルコードを無効化した

--- client/src/app/services/firebase.service.ts
+++ client/src/app/services/firebase.service.ts
@@ -32,34 +32,34 @@ export class FirebaseService {
     });
 
     // of: 指定した複数の値を Observable に変換する
-    of(1, 2, 3).subscribe(x => console.log('of: ' + x));
+    // of(1, 2, 3).subscribe(x => console.log('of: ' + x));
 
     // interval: 指定した時間ごとに値を流す
     // interval(1000).subscribe(console.log);
 
     // concat: Observable 同士を、ストリームの順序を保ったまま結合
-    concat(of(1, 2), of(3, 4)).subscribe(x => console.log('concat: ' + x));
+    // concat(of(1, 2), of(3, 4)).subscribe(x => console.log('concat: ' + x));
 
     // tap: ストリームに影響を与えず、任意の処理を行う
-    of(1 , 2, 3).pipe(
+    /*of(1 , 2, 3).pipe(
         tap(x => console.log('1st: ' + x)),
         tap(x => console.log('2nd: ' + (x * 2))),
         tap(x => console.log('3rd: ' + (x * 3)))
-    ).subscribe(console.log);
+    ).subscribe(console.log);*/
 
     // map, pluck: ストリームの値を加工・変換・抽出する
     // 利用シーン: API のレスポンスを加工する
-    const observable1 = of({ userId: 1, body: 'hoge' });
+    /*const observable1 = of({ userId: 1, body: 'hoge' });
     observable1.pipe(map(x => x.userId)).subscribe(x => console.log('map: ' + x));
-    observable1.pipe(pluck('userId')).subscribe(x => console.log('pluck: ' + x));
+    observable1.pipe(pluck('userId')).subscribe(x => console.log('pluck: ' + x));*/
 
     // mergeMap: 引数を用いて新たな Observable を生成できる
-    of('Hello').pipe(
+    /*of('Hello').pipe(
         mergeMap(val => of(val + ', world!'))
-    ).subscribe(x => console.log('mergeMap: ' + x));
+    ).subscribe(x => console.log('mergeMap: ' + x));*/
 
     // Observable, observer を自作する
-    Observable.create(observer => {
+    /*Observable.create(observer => {
       try {
         observer.next(1);
         observer.next(2);
@@ -71,29 +71,29 @@ export class FirebaseService {
       } catch (err) {
         observer.error();
       }
-    }).subscribe({ /* observer */
+    }).subscribe({
       next(x) { console.log('next: ' + x); },
       error() { console.log('error!'); },
       complete() { console.log('complete!'); }
-    });
+    });*/
 
     // pipe に渡せるオペレータを自作する
-    const square = (input: Observable<number>) =>
+    /*const square = (input: Observable<number>) =>
         Observable.create(observer => input.subscribe({
           next(x) { observer.next(x ** 2); },
           error(error) { observer.error(error); },
           complete() { observer.complete(); }
-        }));
+        }));*/
 
-    from([1, 2, 3, 4])
-        .pipe(square).subscribe(x => console.log('square: ' + x));
+    /*from([1, 2, 3, 4])
+        .pipe(square).subscribe(x => console.log('square: ' + x));*/
 
     // skip: 値のスキップ
-    from([1, 2, 3, 4])
-        .pipe(skip(2)).subscribe(x => console.log('skip: ' + x));
+    /*from([1, 2, 3, 4])
+        .pipe(skip(2)).subscribe(x => console.log('skip: ' + x));*/
 
-    from([1, 2, 3, 4])
+    /*from([1, 2, 3, 4])
         .pipe(filter(x => (x % 2 === 0)))
-        .subscribe(x => console.log('filter: ' + x));
+        .subscribe(x => console.log('filter: ' + x));*/
   }
 }
