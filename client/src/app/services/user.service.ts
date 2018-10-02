51e4d6008d5918a718b84eefe17cb7f78904ebbf
0918nobita <nobita.0918@gmail.com>
2018-10-03 01:19:13 +0900

    :truck: UserService で FirebaseService ではなく AngularFireFunction を DI するようにした

--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -1,23 +1,23 @@
 import { Injectable } from '@angular/core';
+import { AngularFireFunctions } from '@angular/fire/functions';
 
 import { User } from 'shared/entity';
-import { FirebaseService } from './firebase.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  constructor(private firebaseService: FirebaseService) {}
+  constructor(private afFunctions: AngularFireFunctions) {}
 
   async getUserByName(name: string): Promise<User> {
     const response =
-        await this.firebaseService.functions.httpsCallable('getUserByName')(name);
+        await this.afFunctions.functions.httpsCallable('getUserByName')(name);
     if (response.data === null) throw new Error('ユーザーが見つかりません');
     return <User> response.data;
   }
 
   async getUserByUID(uid: string): Promise<User> {
-    const result = await this.firebaseService.functions.httpsCallable('getUserByUID')(uid);
+    const result = await this.afFunctions.functions.httpsCallable('getUserByUID')(uid);
     const data = result.data;
 
     if (data.length === 0) throw new Error('ユーザーが見つかりません');
