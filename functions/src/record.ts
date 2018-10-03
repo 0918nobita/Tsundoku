85ca46e5804a64d3862b572669c249ec2ec2642b
0918nobita <nobita.0918@gmail.com>
2018-10-04 00:00:06 +0900

    :art: Improve readability

--- functions/src/record.ts
+++ functions/src/record.ts
@@ -2,23 +2,20 @@ import { Record } from '../../shared/entity';
 import { Progress } from '../../shared/progress';
 
 export const _getRecordsByISBN = (db: FirebaseFirestore.Firestore) =>
-  (isbn: string) =>
-    db.collection('records')
-      .where('isbn', '==', isbn)
-      .get()
-      .then(querySnapshot => {
-        const records: Record[] = [];
-
-        for (let i = 0; i < querySnapshot.size; i++) {
-          const docData = querySnapshot.docs[i].data();
-          records.push({
-            created: docData.created,
-            desc: docData.desc,
-            isbn,
-            range: Progress.parse(docData.range),
-            user: docData.user
-          });
-        }
-
-        return records;
+  async (isbn: string) => {
+    const querySnapshot = await db.collection('records')
+        .where('isbn', '==', isbn)
+        .get();
+    const records: Record[] = [];
+    for (const doc of querySnapshot.docs) {
+      const docData = doc.data();
+      records.push({
+        created: docData.created,
+        desc: docData.desc,
+        isbn,
+        range: Progress.parse(docData.range),
+        user: docData.user
       });
+    }
+    return records;
+  };
