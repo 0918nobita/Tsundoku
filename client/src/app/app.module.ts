ac3484f93cab5919f250685231f8458c9d596b85
0918nobita <nobita.0918@gmail.com>
2018-10-03 13:02:40 +0900

    :wrench: /bookshelf ページに AuthGuard を設定した
    ログイン済みユーザーしか閲覧できない

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -39,11 +39,11 @@ export const AppRoutes = [
     { path: '', component: TopComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
-    { path: 'bookshelf', component: BookshelfComponent },
     { path: 'bookDetails/:isbn', component: BookDetailsComponent },
     { path: 'profile/:name', component: ProfileComponent },
     { path: 'timeline', component: TimelineComponent }
     { path: 'search', component: SearchComponent, canActivate: [ AuthGuard ] },
+    { path: 'bookshelf', component: BookshelfComponent, canActivate: [ AuthGuard ] },
 ];
 
 @NgModule({
