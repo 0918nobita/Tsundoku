3024a2ddad344238b2f966f76381a9757d0dc4af
0918nobita <nobita.0918@gmail.com>
2018-10-04 00:16:02 +0900

    :recycle: Do refactor

--- functions/src/user.ts
+++ functions/src/user.ts
@@ -1,27 +1,22 @@
 import { User } from '../../shared/entity';
 
 export const _getUserBy = (field: 'name' | 'uid', db: FirebaseFirestore.Firestore) =>
-  (value: string): Promise<User> =>
-    db.collection('users')
-      .where(field, '==', value)
-      .get()
-      .then(querySnapshot => {
-        if (querySnapshot.size === 0) {
-          return null;
-        } else {
-          if (querySnapshot.size > 1)
-            console.warn(`同一の ${field} を持ったユーザーが複数存在しています (value: ${value})`);
-          const docData = querySnapshot.docs[0].data();
-          return {
-            uid: docData.uid,
-            bio: docData.bio,
-            image: docData.image,
-            name: docData.name,
-            screenName: docData.screenName
-          };
-        }
-      })
-      .catch(error => {
-        console.error(error);
-        return null;
-      });
+  async (value: string): Promise<User> => {
+    const querySnapshot = await db.collection('users')
+        .where('field', '==', value)
+        .get();
+    if (querySnapshot.size === 0) {
+      return null;
+    } else {
+      if (querySnapshot.size > 1)
+          console.warn(`同一の ${field} を持ったユーザーが複数存在しています (value: ${value})`);
+      const docData = querySnapshot.docs[0].data();
+      return {
+        uid: docData.uid,
+        bio: docData.bio,
+        image: docData.image,
+        name: docData.name,
+        screenName: docData.screenName
+      };
+    }
+  };
