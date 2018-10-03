e50bb9165cff86fcff536223e3816b05f51949a0
0918nobita <nobita.0918@gmail.com>
2018-10-03 18:50:01 +0900

    :sparkles: NetworkService で、BehaviorSubject を用いてログイン状態を表す真理値を流すようにした

--- client/src/app/services/network.service.ts
+++ client/src/app/services/network.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase/app';
+import { BehaviorSubject, fromEvent, merge } from 'rxjs';
 
 import { firebaseConfig } from '../config';
-import { fromEvent, merge, Observable, of } from 'rxjs';
-import { filter } from 'rxjs/operators';
 
 /**
  * Firebase SDK の設定を各コンポーネントで共有するための、Angular の Service
@@ -12,16 +11,21 @@ import { filter } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class NetworkService {
-  public observable: Observable<boolean | Event>;
+  public behaviorSubject = new BehaviorSubject(navigator.onLine);
+  public observable = this.behaviorSubject.asObservable();
+  private initialized = false;
 
   /** Firebase SDK の設定 */
   constructor() {
-    this.observable = merge(
-        of(navigator.onLine), fromEvent(window, 'online'))
-            .pipe(filter(x => (x !== false)));
+    merge(fromEvent(window, 'online'),
+          fromEvent(window, 'offline')).subscribe(event => {
+            this.behaviorSubject.next(event.type === 'online');
+          });
 
-    this.observable.subscribe(() => {
+    this.observable.subscribe(online => {
+      if (this.initialized || !online) return;
       firebase.initializeApp(firebaseConfig);
+      this.initialized = true;
     });
   }
 }
