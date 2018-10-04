41ced9249a3470eed5c584770662d1af07e43b99
0918nobita <nobita.0918@gmail.com>
2018-10-05 01:45:34 +0900

    :sparkles: UserService の getUserByName メソッドを、AngularFirestore を用いて再実装した

--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -14,10 +14,17 @@ export class UserService {
   }
 
   async getUserByName(name: string): Promise<User> {
-    const response =
-        await this.afFunctions.functions.httpsCallable('getUserByName')(name);
-    if (response.data === null) throw new Error('ユーザーが見つかりません');
-    return <User> response.data;
+    const result = await this.afFirestore
+        .collection<User>('users', ref => ref.where('name', '==', name))
+        .valueChanges().pipe(take(1)).toPromise();
+    if (result.length === 0) throw new Error('ユーザーが見つかりません');
+    return {
+      bio: result[0].bio,
+      image: result[0].image,
+      name,
+      screenName: result[0].screenName,
+      uid: result[0].uid
+    };
   }
 
   async getUserByUID(uid: string): Promise<User> {
