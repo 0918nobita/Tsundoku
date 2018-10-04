03fd401f0623b3d9a031ba6f01963133f42fd33d
0918nobita <nobita.0918@gmail.com>
2018-10-04 17:30:14 +0900

    :recycle: Do refactor

--- client/src/app/services/book.service.ts
+++ client/src/app/services/book.service.ts
@@ -20,68 +20,59 @@ export class BookService {
     this.resolvedBooks = this.dexieService.table('resolvedBooks');
   }
 
-  async getBookByISBN(isbn: string): Promise<ResolvedBook> {
-    return new Promise(async (resolve: (value:  ResolvedBook) => void,
-                              reject:  (reason?: any)          => void) => {
-      const searchBooksOnline = async () =>
-        axios.get(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`)
-          .then(async result => {
-            console.log('Google Book API を用いて検索します');
+  async getBookByISBN(isbn: string): Promise<ResolvedBook | null> {
+    const localBooks = await this.resolvedBooks.where('isbn').equals(isbn).toArray();
+    if (localBooks.length > 0) {
+      return localBooks[0];
+    } else if (navigator.onLine) {
+      return await this.searchBookOnline(isbn);
+    } else {
+      console.log('オフライン状態なので、検索ができませんでした');
+      return null;
+    }
+  }
 
-            const searchBooksInFirestore = (clue: string): Promise<ResolvedBook[]> =>
-              this.functions.httpsCallable('searchBooksByISBN')({isbn: clue, usingGoogleBooksAPI: false})
-                .then(response => response.data)
-                .catch(error => error);
+  private async searchBooksInFirestore(clue: string): Promise<ResolvedBook[]> {
+    return (await this.functions.httpsCallable('searchBooks')(
+        {isbn: clue, usingGoogleBooksAPI: false})).data;
+  }
 
-            let hitBooks: ResolvedBook[] = [];
+  private async searchBookOnline(isbn: string): Promise<ResolvedBook | null> {
+    const result = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn}`);
 
-            if (result.data.totalItems > 0) {
-              // ヒットした場合は取り出してサムネを出力する
-              for (let i = 0; i < result.data.items.length; i++) {
-                const volumeInfo = result.data.items[i].volumeInfo,
-                      hitBook: ResolvedBook = {
-                        desc: volumeInfo.description,
-                        donor: 'none',
-                        image: './assets/image_not_found.png',
-                        isbn,
-                        title: volumeInfo.title,
-                        pageCount: volumeInfo.pageCount
-                      };
+    let hitBooks: ResolvedBook[] = [];
+    if (result.data.totalItems > 0) {
+      for (const item of result.data.items) {
+        const volumeInfo = item.volumeInfo,
+            hitBook: ResolvedBook = {
+              desc: volumeInfo.description,
+              donor: 'none',
+              image: './assets/image_not_found.png',
+              isbn,
+              title: volumeInfo.title,
+              pageCount: volumeInfo.pageCount
+            };
 
-                if (volumeInfo.imageLinks === void 0) {
-                  const books = await searchBooksInFirestore(isbn);
-                  if (books.length > 0)
-                    Object.assign(hitBook, { donor: books[0].donor, image: books[0].image });
-                } else {
-                  hitBook.image = `https${volumeInfo.imageLinks.smallThumbnail.slice(4)}`;
-                }
+        if (volumeInfo.imageLinks === void 0) {
+          const books = await this.searchBooksInFirestore(isbn);
+          if (books.length > 0)
+            Object.assign(hitBook, { donor: books[0].donor, image: books[0].image });
+        } else {
+          hitBook.image = `https${volumeInfo.imageLinks.smallThumbnail.slice(4)}`;
+        }
 
-                hitBooks.push(hitBook);
-              }
-            } else {
-              // ヒットしなかった場合は resolvedBooks で検索する
-              hitBooks = await searchBooksInFirestore(isbn);
-            }
-
-            if (hitBooks.length > 0) {
-              // データベースに本の情報を登録する
-              await this.resolvedBooks.add(hitBooks[0]);
-              resolve(hitBooks[0]);
-            } else {
-              reject();
-            }
-          })
-          .catch(error => reject(error));
-
-      const localBooks = await this.resolvedBooks.where('isbn').equals(isbn).toArray();
-      if (localBooks.length > 0) {
-        resolve(localBooks[0]);
-      } else if (navigator.onLine) {
-        await searchBooksOnline();
-      } else {
-        console.log('オフライン状態なので、検索ができませんでした');
-        reject();
+        hitBooks.push(hitBook);
       }
-    });
+    } else {
+      hitBooks = await this.searchBooksInFirestore(isbn);
+    }
+
+    if (hitBooks.length > 0) {
+      // データベースに本の情報を登録する
+      await this.resolvedBooks.add(hitBooks[0]);
+      return hitBooks[0];
+    } else {
+      return null;
+    }
   }
 }
