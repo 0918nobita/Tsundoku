a599e95b5db46fb6405ad86d6cc309ca513f6b66
0918nobita <nobita.0918@gmail.com>
2018-10-05 14:00:08 +0900

    :fire: Functions側のgetRecordsByISBN関数を削除した

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,6 @@ import * as functions from 'firebase-functions';
 
 import { _checkConnectionFrom, _getUsersConnectedFrom } from './connection';
 import { _searchBooksByISBN, _postResolvedBook } from './book';
-import { _getRecordsByISBN } from './record';
 
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
@@ -14,5 +13,3 @@ export const getUsersConnectedFrom = functions.https.onCall(_getUsersConnectedFr
 
 export const searchBooksByISBN = functions.https.onCall(_searchBooksByISBN(db));
 export const postResolvedBook = functions.https.onCall(_postResolvedBook(db));
-
-export const getRecordsByISBN = functions.https.onCall(_getRecordsByISBN(db));
