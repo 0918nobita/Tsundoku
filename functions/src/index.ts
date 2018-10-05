9ea87ccc25a2b82ccd0c9cfb41983ade159dcc0d
0918nobita <nobita.0918@gmail.com>
2018-10-05 13:59:17 +0900

    :fire: Functions側のgetUserByName関数を削除した

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,6 @@
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 
-import { _getUserBy } from './user';
 import { _checkConnectionFrom, _getUsersConnectedFrom } from './connection';
 import { _searchBooksByISBN, _postResolvedBook } from './book';
 import { _getRecordsByISBN } from './record';
@@ -9,8 +8,6 @@ import { _getRecordsByISBN } from './record';
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 db.settings({ timestampsInSnapshots: true });
-
-export const getUserByName = functions.https.onCall(_getUserBy('name', db));
 
 export const checkConnectionFrom = functions.https.onCall(_checkConnectionFrom(db));
 export const getUsersConnectedFrom = functions.https.onCall(_getUsersConnectedFrom(db));
