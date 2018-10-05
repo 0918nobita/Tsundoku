02b5396af9861d8496af6835e6841781c3461925
0918nobita <nobita.0918@gmail.com>
2018-10-05 10:21:21 +0900

    :sparkles: RecordService に、getRecordsByISBN メソッドを追加した

--- client/src/app/services/record.service.ts
+++ client/src/app/services/record.service.ts
@@ -1,8 +1,35 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { take } from 'rxjs/operators';
+import * as firebase from 'firebase';
+
+import { Record } from 'shared/entity';
+import { Progress } from 'shared/progress';
+
+interface RecordInFirestore {
+  created: firebase.firestore.Timestamp;
+  desc: string;
+  isbn: string;
+  range: string;
+  user: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecordService {
-  constructor() {}
+  constructor(private afFirestore: AngularFirestore) {}
+
+  async getRecordsByISBN(isbn: string): Promise<Record[]> {
+    const records = await this.afFirestore
+        .collection<RecordInFirestore>('records', ref => ref.where('isbn', '==', isbn))
+        .valueChanges().pipe(take(1)).toPromise();
+    return records.map(item => (<Record> {
+        created: item.created,
+        desc: item.desc,
+        isbn: item.isbn,
+        range: Progress.parse(item.range),
+        user: item.user
+      }));
+  }
 }
