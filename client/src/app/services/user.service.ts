f3739af11ec8ce025bed420f50be0dcd3cdc9633
0918nobita <nobita.0918@gmail.com>
2018-10-05 15:44:28 +0900

    :recycle: Do refactor

--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { AngularFireFunctions } from '@angular/fire/functions';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { take } from 'rxjs/operators';
 
@@ -9,21 +8,14 @@ import { User } from 'shared/entity';
   providedIn: 'root'
 })
 export class UserService {
-  constructor(private afFunctions: AngularFireFunctions,
-              private afFirestore: AngularFirestore) {}
+  constructor(private afFirestore: AngularFirestore) {}
 
   async getUserByName(name: string): Promise<User> {
     const result = await this.afFirestore
         .collection<User>('users', ref => ref.where('name', '==', name))
         .valueChanges().pipe(take(1)).toPromise();
     if (result.length === 0) throw new Error('ユーザーが見つかりません');
-    return {
-      bio: result[0].bio,
-      image: result[0].image,
-      name,
-      screenName: result[0].screenName,
-      uid: result[0].uid
-    };
+    return { ...result[0] };
   }
 
   async getUserByUID(uid: string): Promise<User> {
@@ -31,12 +23,6 @@ export class UserService {
         .collection<User>('users', ref => ref.where('uid', '==', uid))
         .valueChanges().pipe(take(1)).toPromise();
     if (result.length === 0) throw new Error('ユーザーが見つかりません');
-    return {
-      bio: result[0].bio,
-      image: result[0].image,
-      name: result[0].name,
-      screenName: result[0].screenName,
-      uid
-    };
+    return { ...result[0] };
   }
 }
