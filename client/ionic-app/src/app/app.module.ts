5f0edf31e4f30120bd7aef27f3ddf49aabc443c0
0918nobita <nobita.0918@gmail.com>
2018-10-19 00:48:20 +0900

    :truck: home パッケージの名称を bookshelf に変更した

--- client/ionic-app/src/app/app.module.ts
+++ client/ionic-app/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+import { HomePage } from '../pages/bookshelf/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
--- client/ionic-app/src/pages/tabs/tabs.ts
+++ client/ionic-app/src/pages/tabs/tabs.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { HomePage } from '../bookshelf/home';
 
 @Component({
   templateUrl: 'tabs.html'
