f6792febe2c00de1fdef3feb06b7a621ef1ec179
0918nobita <nobita.0918@gmail.com>
2018-10-19 01:05:15 +0900

    :truck: contact.html → achievement.html

--- client/ionic-app/src/pages/achievement/contact.ts
+++ client/ionic-app/src/pages/achievement/contact.ts
@@ -3,7 +3,7 @@ import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-contact',
-  templateUrl: 'contact.html'
+  templateUrl: 'achievement.html'
 })
 export class ContactPage {
 
