a55b0245118dfa5a3e10a579cc147b90da24bd3d
0918nobita <nobita.0918@gmail.com>
2018-10-19 12:36:20 +0900

    :pencil2: Fix typos

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
-echo $API_KEY
 cd client/ionic-app
 touch src/app/config.ts
-echo "
-  export const firebaseConfig = {
-    apiKey: \"$API_KEY\"
-  }
-" | sed -e 's/^ *//g' -e '1d' -e '$d'
+{
+  echo "export const firebaseConfig = {"
+  echo "  apiKey: \"$API_KEY\","
+  echo "  authDomain: \"$AUTH_DOMAIN\","
+  echo "  databaseUrl: \"$DATABASE_URL\","
+  echo "  messagingSenderId: \"$MESSAGING_SENDER_ID\","
+  echo "  projectId: \"$PROJECT_ID\","
+  echo "  storageBucket: \"$STORAGE_BUCKET\""
+  echo "}"
+} >> src/app/config.ts
 cat src/app/config.ts
 npm i
 npm run release
