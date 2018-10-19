94d2e2ab212e46c33e469f8aba9594a0b6c0f875
0918nobita <nobita.0918@gmail.com>
2018-10-19 12:26:00 +0900

    :construction: /build.sh に、config.ts を生成する処理を追加した

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 echo $API_KEY
 cd client/ionic-app
+touch src/app/config.ts
+echo "
+  export const firebaseConfig = {
+    apiKey: \"$API_KEY\"
+  }
+" | sed -e 's/^ *//g' -e '1d' -e '$d'
+cat src/app/config.ts
 npm i
 npm run release
