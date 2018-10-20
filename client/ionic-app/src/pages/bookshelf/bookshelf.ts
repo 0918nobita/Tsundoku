30e2232e13e376cfe729ab68155c46b2132096db
0918nobita <nobita.0918@gmail.com>
2018-10-20 18:32:59 +0900

    :construction: 本棚ページでユーザー「0918nobita」の本棚のサムネイル一覧が表示されるようにした

--- client/ionic-app/src/pages/bookshelf/bookshelf.ts
+++ client/ionic-app/src/pages/bookshelf/bookshelf.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { RegisteredBook } from '../../../../../shared/entity';
+import { BookshelfService } from '../../app/services/bookshelf.service';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'bookshelf.html'
 })
 export class BookshelfPage {
-  constructor(public navCtrl: NavController) {}
+  registeredBooks: RegisteredBook[] = [];
+
+  constructor(
+    public navCtrl: NavController,
+    private bookshelfService: BookshelfService
+  ) {}
+
+  ionViewDidLoad() {
+    this.bookshelfService.getBookshelf('0918nobita').subscribe(book => {
+      if (this.registeredBooks.indexOf(book) === -1)
+        this.registeredBooks.push(book);
+    });
+  }
 }
