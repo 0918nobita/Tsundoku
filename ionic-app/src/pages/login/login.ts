c36d9e7575db2db6a4da4a902ee7c26baea6873f
0918nobita <nobita.0918@gmail.com>
2018-10-22 01:35:05 +0900

    :sparkles: ログインページに、GitHubログイン用のボタンを追加した

--- ionic-app/src/pages/login/login.ts
+++ ionic-app/src/pages/login/login.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController, ToastController } from 'ionic-angular';
-import { AngularFireAuth } from '@angular/fire/auth';
-import * as firebase from 'firebase/app';
-import { OAuthCredential } from '@firebase/auth-types';
+import { NavController } from 'ionic-angular';
 
-// import { TabsPage } from '../tabs/tabs';
 import { SignUpPage } from '../signup/signup';
 import { gitHubConfig } from '../../app/config';
 
@@ -13,38 +9,12 @@ import { gitHubConfig } from '../../app/config';
   templateUrl: 'login.html'
 })
 export class LoginPage {
-  login: {
-    email: string;
-    password: string;
-  } = {
-    email: '',
-    password: ''
-  };
+  constructor(public navCtrl: NavController) {}
 
-  constructor(
-    public navCtrl: NavController,
-    private toastCtrl: ToastController,
-    private afAuth: AngularFireAuth
-  ) {}
-
-  async userLogin() {
-    try {
-      const provider = new firebase.auth.GithubAuthProvider();
-      provider.addScope('repo');
-      provider.setCustomParameters({
-        "client_id": gitHubConfig.clientId
-      });
-      const { user, credential }: { user: firebase.User, credential: OAuthCredential } =
-        await this.afAuth.auth.signInWithPopup(provider);
-      console.log('user: ', user);
-      console.log('credential: ', credential);
-    } catch (error) {
-      await this.toastCtrl
-        .create({
-          message: error,
-          duration: 5000
-        }).present();
-    }
+  userLogin() {
+    location.href = `https://github.com/login/oauth/authorize?client_id=${
+      gitHubConfig.clientId
+    }&scope=repo`;
   }
 
   async gotoSignUp() {
