2e08077bb44d9594862b19cc80c15d2c46097088
0918nobita <nobita.0918@gmail.com>
2018-10-21 21:38:56 +0900

    :sparkles: /build.sh に、自動生成する config.ts で定数 gitHubConfig を export するようにコマンドを追加した

--- build.sh
+++ build.sh
@@ -9,7 +9,11 @@ cd ../ionic-app
   echo "  messagingSenderId: \"$MESSAGING_SENDER_ID\","
   echo "  projectId: \"$PROJECT_ID\","
   echo "  storageBucket: \"$STORAGE_BUCKET\""
-  echo "}"
+  echo "};"
+  echo "export const gitHubConfig = {"
+  echo "  clientId: \"$GITHUB_CLIENT_ID\","
+  echo "  clientSecret: \"$GITHUB_CLIENT_SECRET\""
+  echo "};"
 } >> src/app/config.ts
 npm i
 npm run release
