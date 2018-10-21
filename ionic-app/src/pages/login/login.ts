b5b11a77a2786353b535e9a73c5cd0a7785771a9
0918nobita <nobita.0918@gmail.com>
2018-10-21 21:39:18 +0900

    :construction: GitHub アカウントでログインするための provider を生成する処理を追加した

--- ionic-app/src/pages/login/login.ts
+++ ionic-app/src/pages/login/login.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController, ToastController } from 'ionic-angular';
 import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase/app';
+import { OAuthCredential } from '@firebase/auth-types';
 
-import { TabsPage } from '../tabs/tabs';
+// import { TabsPage } from '../tabs/tabs';
 import { SignUpPage } from '../signup/signup';
+import { gitHubConfig } from '../../app/config';
 
 @Component({
   selector: 'page-login',
@@ -26,19 +29,21 @@ export class LoginPage {
 
   async userLogin() {
     try {
-      await this.afAuth.auth.signInWithEmailAndPassword(
-        this.login.email,
-        this.login.password
-      );
-
-      await this.navCtrl.setRoot(TabsPage);
+      const provider = new firebase.auth.GithubAuthProvider();
+      provider.addScope('repo');
+      provider.setCustomParameters({
+        "client_id": gitHubConfig.clientId
+      });
+      const { user, credential }: { user: firebase.User, credential: OAuthCredential } =
+        await this.afAuth.auth.signInWithPopup(provider);
+      console.log('user: ', user);
+      console.log('credential: ', credential);
     } catch (error) {
       await this.toastCtrl
         .create({
           message: error,
           duration: 5000
-        })
-        .present();
+        }).present();
     }
   }
 
