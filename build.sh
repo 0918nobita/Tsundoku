abdb7b81de708cbdc065b028538577249adab362
0918nobita <nobita.0918@gmail.com>
2018-10-23 22:04:33 +0900

    :pencil2: Fix typo

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ cd ../ionic-app
   echo "export const firebaseConfig = {"
   echo "  apiKey: \"$API_KEY\","
   echo "  authDomain: \"$AUTH_DOMAIN\","
-  echo "  databaseUrl: \"$DATABASE_URL\","
+  echo "  databaseURL: \"$DATABASE_URL\","
   echo "  messagingSenderId: \"$MESSAGING_SENDER_ID\","
   echo "  projectId: \"$PROJECT_ID\","
   echo "  storageBucket: \"$STORAGE_BUCKET\""
