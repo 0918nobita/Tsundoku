49d6a729ef1bd965adfcfaa636f75987ccc44db0
0918nobita <nobita.0918@gmail.com>
2018-10-27 13:38:43 +0900

    :fire: checkConnectionFrom, getUsersConnectedFrom 関数を削除した

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,11 @@
 import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
 
-import { _checkConnectionFrom, _getUsersConnectedFrom } from "./connection";
 import { _searchBooksByISBN, _postResolvedBook } from "./book";
 
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 db.settings({ timestampsInSnapshots: true });
-
-export const checkConnectionFrom = functions.https.onCall(
-  _checkConnectionFrom(db)
-);
-export const getUsersConnectedFrom = functions.https.onCall(
-  _getUsersConnectedFrom(db)
-);
 
 export const searchBooksByISBN = functions.https.onCall(_searchBooksByISBN(db));
 export const postResolvedBook = functions.https.onCall(_postResolvedBook(db));
