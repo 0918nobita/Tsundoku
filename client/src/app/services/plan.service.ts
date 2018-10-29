4d9c2c7bc9021010b454d894596486b740a07851
0918nobita <nobita.0918@gmail.com>
2018-10-29 19:45:57 +0900

    :sparkles: PlanService クラスに getPlans メソッドを追加した

--- client/src/app/services/plan.service.ts
+++ client/src/app/services/plan.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@angular/core';
+import { from, Observable } from "rxjs";
+import { flatMap } from "rxjs/operators";
+import { AngularFirestore } from "@angular/fire/firestore";
+
 import { Plan, ResolvedBook } from "shared/entity";
+import { BookService } from "./book.service";
 
 export interface DetailedPlan extends Plan {
   book: ResolvedBook
@@ -9,5 +14,24 @@ export interface DetailedPlan extends Plan {
   providedIn: 'root'
 })
 export class PlanService {
-  constructor() {}
+  constructor(
+    private afFirestore: AngularFirestore,
+    private bookService: BookService
+  ) {}
+
+  getPlans(uid: string): Observable<DetailedPlan> {
+    const attachBookDetails = (plan: Plan) =>
+      this.bookService.getBookByISBN(plan.isbn).then(result =>
+        <DetailedPlan>{ ...plan, book: {...result}});
+
+    const nextDetailedPlans = (plans: Plan[]) =>
+      from(plans).pipe(
+        flatMap(plan => from(attachBookDetails(plan)))
+      );
+
+    return this.afFirestore
+      .collection<Plan>('plans', ref => ref.where('uid', '==', uid))
+      .valueChanges()
+      .pipe(flatMap(nextDetailedPlans));
+  }
 }
