23d7ca76d49af4e73261730ec2572c013ae95fc1
0918nobita <nobita.0918@gmail.com>
2018-10-29 18:44:47 +0900

    :sparkles: 達成ページでスキル一覧を表示するようにした

--- client/src/pages/achievement/achievement.ts
+++ client/src/pages/achievement/achievement.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import * as firebase from 'firebase/app';
 import 'firebase/auth';
+
+import { SkillService } from '../../app/services/skill.service';
+import { Skill } from 'shared/entity';
 
 @Component({
   selector: 'page-contact',
   templateUrl: 'achievement.html'
 })
 export class AchievementPage {
-  constructor(public navCtrl: NavController) {}
+  skills: Skill[] = [];
 
-  ionViewWillEnter() {}
+  constructor(
+    public navCtrl: NavController,
+    private skillService: SkillService
+  ) {}
+
+  ionViewWillEnter() {
+    this.skillService
+      .getSkills(firebase.auth().currentUser.uid)
+      .subscribe(skill => {
+        if (
+          this.skills.filter(
+            item => item.created.toMillis() === skill.created.toMillis()
+          ).length === 0
+        )
+          this.skills.push(skill);
+      });
+  }
 }
