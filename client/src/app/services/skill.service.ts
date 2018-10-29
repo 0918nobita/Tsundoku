364d313db6bb7bd6790708e03446d693596cafff
0918nobita <nobita.0918@gmail.com>
2018-10-29 17:49:17 +0900

    :sparkles: SkillService クラスに getSkills メソッドを追加した

--- client/src/app/services/skill.service.ts
+++ client/src/app/services/skill.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { flatMap } from 'rxjs/operators';
+import { from, Observable } from 'rxjs';
+
+import { Skill } from '../../../../shared/entity';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SkillService {
-  constructor() {}
+  constructor(private afFirestore: AngularFirestore) {}
+
+  getSkills(uid: string): Observable<Skill> {
+    return this.afFirestore
+      .collection<Skill>('skills', ref => ref.where('uid', '==', uid))
+      .valueChanges()
+      .pipe(flatMap(skills => from(skills)));
+  }
 }
