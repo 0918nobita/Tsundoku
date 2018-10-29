d0609b378e0c53126c4960e70d3e3ad8acf8235c
0918nobita <nobita.0918@gmail.com>
2018-10-29 17:17:50 +0900

    :sparkles: registerBook 関数の呼び出し時に、created, modified フィールドも設定するようにした

--- functions/src/bookshelf.ts
+++ functions/src/bookshelf.ts
@@ -1,6 +1,13 @@
+import * as admin from "firebase-admin";
+
 export const _registerBook = (db: FirebaseFirestore.Firestore) => async (args: {
   isbn: string;
   uid: string;
 }) => {
-  return (await db.collection("bookshelf").add(args)).id;
+  const date = admin.firestore.FieldValue.serverTimestamp();
+  return (await db.collection("bookshelf").add({
+    ...args,
+    created: date,
+    modified: date
+  })).id;
 };
