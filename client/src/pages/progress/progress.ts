ae21c33ef7bb573227ddb297f655a0d856e7189c
0918nobita <nobita.0918@gmail.com>
2018-10-29 19:50:44 +0900

    :sparkles: 進捗ページで、PlanService を用いて読書計画を取得し表示するようにした

--- client/src/pages/progress/progress.ts
+++ client/src/pages/progress/progress.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { DetailedPlan, PlanService } from '../../app/services/plan.service';
+import * as firebase from 'firebase';
+
 @Component({
   selector: 'page-about',
   templateUrl: 'progress.html'
 })
 export class ProgressPage {
-  constructor(public navCtrl: NavController) {}
+  plans: DetailedPlan[] = [];
+
+  constructor(
+    public navCtrl: NavController,
+    private planService: PlanService
+  ) {}
+
+  ionViewWillEnter() {
+    this.planService
+      .getPlans(firebase.auth().currentUser.uid)
+      .subscribe(plan => {
+        if (
+          this.plans.filter(
+            item => item.modified.toMillis() === plan.modified.toMillis()
+          ).length === 0
+        )
+          this.plans.push(plan);
+      });
+  }
 }
