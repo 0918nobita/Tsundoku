4c07c9b9780584351891f7943b726e752cdadf81
0918nobita <nobita.0918@gmail.com>
2018-10-29 19:46:42 +0900

    :art: Prettier を用いてコードフォーマットを行った

--- client/src/app/services/plan.service.ts
+++ client/src/app/services/plan.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
-import { from, Observable } from "rxjs";
-import { flatMap } from "rxjs/operators";
-import { AngularFirestore } from "@angular/fire/firestore";
+import { from, Observable } from 'rxjs';
+import { flatMap } from 'rxjs/operators';
+import { AngularFirestore } from '@angular/fire/firestore';
 
-import { Plan, ResolvedBook } from "shared/entity";
-import { BookService } from "./book.service";
+import { Plan, ResolvedBook } from 'shared/entity';
+import { BookService } from './book.service';
 
 export interface DetailedPlan extends Plan {
-  book: ResolvedBook
+  book: ResolvedBook;
 }
 
 @Injectable({
@@ -21,13 +21,12 @@ export class PlanService {
 
   getPlans(uid: string): Observable<DetailedPlan> {
     const attachBookDetails = (plan: Plan) =>
-      this.bookService.getBookByISBN(plan.isbn).then(result =>
-        <DetailedPlan>{ ...plan, book: {...result}});
+      this.bookService
+        .getBookByISBN(plan.isbn)
+        .then(result => <DetailedPlan>{ ...plan, book: { ...result } });
 
     const nextDetailedPlans = (plans: Plan[]) =>
-      from(plans).pipe(
-        flatMap(plan => from(attachBookDetails(plan)))
-      );
+      from(plans).pipe(flatMap(plan => from(attachBookDetails(plan))));
 
     return this.afFirestore
       .collection<Plan>('plans', ref => ref.where('uid', '==', uid))
