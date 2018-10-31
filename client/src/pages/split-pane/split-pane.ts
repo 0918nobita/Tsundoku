6fef93ac8bc4ed2518b205fc1221483d25f547dd
0918nobita <nobita.0918@gmail.com>
2018-10-31 19:35:05 +0900

    :sparkles: サイドバーに、タブを切り替えるためのボタンを追加した

--- client/src/pages/split-pane/split-pane.ts
+++ client/src/pages/split-pane/split-pane.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 import { TabsPage } from '../tabs/tabs';
+import { Nav } from 'ionic-angular';
 
 @Component({
   selector: 'split-pane',
   templateUrl: 'split-pane.html'
 })
 export class SplitPane {
+  @ViewChild(Nav)
+  navRef: Nav;
   root = TabsPage;
 
   constructor() {}
 
   ionViewWillEnter() {}
+
+  showProgressPage() {
+    this.navRef.getActiveChildNav().select(1);
+  }
+
+  showBookshelfPage() {
+    this.navRef.getActiveChildNav().select(0);
+  }
+
+  showAchievementPage() {
+    this.navRef.getActiveChildNav().select(2);
+  }
 }
