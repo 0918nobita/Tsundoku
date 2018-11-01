5a55317c844feea25a2b533aa498c13211fb3c59
0918nobita <nobita.0918@gmail.com>
2018-11-01 12:34:12 +0900

    :wrench: Jest の設定を package.json 内に記述するようにした

--- client/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: 'jest-preset-angular',
-  roots: ['src'],
-  testMatch: ['\\.spec\\.ts$'],
-  setupTestFrameworkScriptFile: '<rootDir>/src/setupJest.ts',
-  transformIgnorePatterns: ['node_modules/(?!@ngrx|@ionic-native|@ionic)']
-};
