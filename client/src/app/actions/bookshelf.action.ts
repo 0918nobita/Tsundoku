aada569dcfaa008dc70966cfad6ffebdd10529c7
0918nobita <nobita.0918@gmail.com>
2018-11-04 23:56:23 +0900

    :construction: CreateBook アクションを追加した

--- client/src/app/actions/bookshelf.action.ts
+++ client/src/app/actions/bookshelf.action.ts
@@ -1,7 +1,16 @@
+import { Action } from '@ngrx/store';
+import { RegisteredBook } from '../models/registered-book';
+
 // Action の種類
 export enum BookshelfActionTypes {
   CreateBook = '[Bookshelf] Create book',
   SearchBookByIsbn = '[Bookshelf] Search book by ISBN',
   SearchBookBySkill = '[Bookshelf] Search book by skill',
   RegisterResolvedBook = '[Bookshelf] Register resolved book'
+}
+
+export class CreateBook implements Action {
+  readonly type = BookshelfActionTypes.CreateBook;
+
+  constructor(public payload: RegisteredBook) {}
 }
