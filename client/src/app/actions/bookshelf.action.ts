3343fc2136f5df8b6c539d590a6019447d362ddd
0918nobita <nobita.0918@gmail.com>
2018-11-04 23:55:53 +0900

    :construction: BookshelfActionTypes 列挙型を追加した

---
+++ client/src/app/actions/bookshelf.action.ts
@@ -0,0 +1,7 @@
+// Action の種類
+export enum BookshelfActionTypes {
+  CreateBook = '[Bookshelf] Create book',
+  SearchBookByIsbn = '[Bookshelf] Search book by ISBN',
+  SearchBookBySkill = '[Bookshelf] Search book by skill',
+  RegisterResolvedBook = '[Bookshelf] Register resolved book'
+}
