9c8189c02d698c2d8969d333feb325bb9d33004f
0918nobita <nobita.0918@gmail.com>
2018-11-04 14:35:36 +0900

    :art: BookDetailsModal が FundamentalModal を継承するようにした
    dismiss メソッドの定義が不要になった

--- client/src/pages/book-details-modal/book-details-modal.ts
+++ client/src/pages/book-details-modal/book-details-modal.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { NavParams, ViewController } from 'ionic-angular';
 
 import { BookService } from '../../app/services/book.service';
+import { FundamentalModal } from '../fundamental-modal';
 
 @Component({
   selector: 'book-details-modal',
   templateUrl: 'book-details-modal.html'
 })
-export class BookDetailsModal {
+export class BookDetailsModal extends FundamentalModal {
   isbn: string;
   title: string;
   desc: string;
@@ -15,18 +16,15 @@ export class BookDetailsModal {
 
   constructor(
     private navParams: NavParams,
-    private viewCtrl: ViewController,
+    protected viewCtrl: ViewController,
     private bookService: BookService
   ) {
+    super(viewCtrl);
     this.isbn = this.navParams.get('isbn');
     this.bookService.getBookByISBN(this.isbn).then(book => {
       this.title = book.title;
       this.desc = book.desc;
       this.image = book.image;
     });
-  }
-
-  async dismiss() {
-    await this.viewCtrl.dismiss();
   }
 }
