264cb8702430138f01d54cd5fa1fcb7b160ea73b
0918nobita <nobita.0918@gmail.com>
2018-11-04 13:59:05 +0900

    :sparkles: sortByModifiedDatetime 関数を firestore.service.ts に追加した

--- client/src/app/services/firestore.service.ts
+++ client/src/app/services/firestore.service.ts
@@ -3,3 +3,22 @@ import 'firebase/auth';
 
 export const mine = (ref: firebase.firestore.Query) =>
   ref.where('uid', '==', firebase.auth().currentUser.uid);
+
+export const sortByModifiedDatetime = (
+  items: { modified: firebase.firestore.Timestamp }[],
+  order: 'asc' | 'desc'
+) => {
+  items.sort((former, latter) => {
+    const asc = order === 'asc',
+      formerMillis = former.modified.toMillis(),
+      latterMillis = latter.modified.toMillis();
+
+    if (formerMillis < latterMillis) {
+      return asc ? -1 : 1;
+    } else if (formerMillis > latterMillis) {
+      return asc ? 1 : -1;
+    }
+
+    return 0;
+  });
+};
