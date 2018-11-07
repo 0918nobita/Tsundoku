f4d3c3c8d64bb0e44752597c81b2ba6e8086c400
0918nobita <nobita.0918@gmail.com>
2018-11-07 17:45:37 +0900

    :sparkles: PlanActionTypes に対する Reducer を追加した

---
+++ client/src/app/state/plan.reducer.ts
@@ -0,0 +1,16 @@
+import { Action } from "@ngrx/store";
+
+import { initialState } from "./_state.inits";
+import { State } from "./_state.interfaces";
+import { PlanActionTypes } from "./plan.action";
+
+export function reducer(state = initialState, action: Action): State {
+  switch (action.type) {
+    case PlanActionTypes.CreatePlan:
+      return state;
+    case PlanActionTypes.UpdatePlan:
+      return state;
+    case PlanActionTypes.DeletePlan:
+      return state;
+  }
+}
