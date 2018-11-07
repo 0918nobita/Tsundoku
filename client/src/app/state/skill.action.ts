ea199af9d5a2f6e4b4ab0abfa659195ec052e0d4
0918nobita <nobita.0918@gmail.com>
2018-11-07 17:49:45 +0900

    :sparkles: スキルの管理に関する Action を追加した

---
+++ client/src/app/state/skill.action.ts
@@ -0,0 +1,16 @@
+import { Action } from '@ngrx/store';
+
+export enum SkillActionTypes {
+  CreateSkill = '[Skill] Create',
+  DeleteSkill = '[Skill] Delete'
+}
+
+class CreateSkill implements Action {
+  readonly type = SkillActionTypes.CreateSkill;
+}
+
+class DeleteSkill implements Action {
+  readonly type = SkillActionTypes.DeleteSkill;
+}
+
+type PlanActions = CreateSkill | DeleteSkill;
