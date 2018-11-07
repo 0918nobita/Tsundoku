f4dbc552c5e9142d1a31c603682da34c28bd6fa7
0918nobita <nobita.0918@gmail.com>
2018-11-07 14:53:42 +0900

    :sparkles: RegisteredBook インターフェースを追加した

--- client/src/app/models/registered-book.ts
+++ client/src/app/models/registered-book.ts
@@ -1,3 +1,8 @@
-export class RegisteredBook {
-  constructor() {}
+import * as firebase from 'firebase/app';
+
+import { ResolvedBook } from './resolved-book';
+
+export interface RegisteredBook extends ResolvedBook {
+  created: firebase.firestore.Timestamp;
+  modified: firebase.firestore.Timestamp;
 }
