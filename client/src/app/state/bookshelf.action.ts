9febea3772aca19fa0c1fa22b57419038ca8d743
0918nobita <nobita.0918@gmail.com>
2018-11-07 17:20:22 +0900

    ✨ 本棚の管理に関する Action を追加した

---
+++ client/src/app/state/bookshelf.action.ts
@@ -0,0 +1,28 @@
+import { Action } from '@ngrx/store';
+
+/*
+  本棚に関するアクション
+  - 本棚に手動入力の本を追加する
+  - 本棚に解決済みの本を追加する
+  - 本棚の一括削除 (退会時)
+  */
+
+export enum BookshelfActionTypes {
+  RegisterManuallyAddedBook = '[Bookshelf] Register manually added book',
+  RegisterResolvedBook = '[Bookshelf] Register resolved book',
+  DeleteBookshelf = '[Bookshelf] Delete'
+}
+
+export class RegisterManuallyAddedBook implements Action {
+  readonly type = BookshelfActionTypes.RegisterManuallyAddedBook;
+}
+
+export class RegisterResolvedBook implements Action {
+  readonly type = BookshelfActionTypes.RegisterResolvedBook;
+}
+
+export class DeleteBookshelf implements Action {
+  readonly type = BookshelfActionTypes.DeleteBookshelf;
+}
+
+type BookshelfActions = RegisterManuallyAddedBook | RegisterResolvedBook | DeleteBookshelf;
