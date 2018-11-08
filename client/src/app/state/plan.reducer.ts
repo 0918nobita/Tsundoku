e3d1b08515f3ff1f7f95b67737c2d51175fbbc12
0918nobita <nobita.0918@gmail.com>
2018-11-08 18:13:26 +0900

    :sparkles: WatchPlanFail アクションに対する Reducer を追加した

--- client/src/app/state/plan.reducer.ts
+++ client/src/app/state/plan.reducer.ts
@@ -2,7 +2,7 @@ import { Action } from '@ngrx/store';
 
 import { initialPlanState } from './_state.inits';
 import { PlanState } from './_state.interfaces';
-import { PlanActionTypes, UpdatePlan } from './plan.action';
+import { PlanActionTypes, UpdatePlan, WatchPlanFail } from './plan.action';
 import {
   updateDynamicList,
   sortByDatetime
@@ -11,9 +11,12 @@ import {
 export function reducer(state = initialPlanState, action: Action): PlanState {
   switch (action.type) {
     case PlanActionTypes.WatchPlan:
-    case PlanActionTypes.WatchPlanFail:
     case PlanActionTypes.CreatePlan:
     case PlanActionTypes.DeletePlan:
+      return state;
+
+    case PlanActionTypes.WatchPlanFail:
+      console.error((action as WatchPlanFail).payload.error);
       return state;
 
     case PlanActionTypes.UpdatePlan:
