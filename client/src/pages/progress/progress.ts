9df3d46fe3f009e7a3bb70cca29f89a53968b14a
0918nobita <nobita.0918@gmail.com>
2018-11-11 00:43:55 +0900

    :art: PlanService ではなく Store から読書計画のデータを取得して出力するようにした

--- client/src/pages/progress/progress.ts
+++ client/src/pages/progress/progress.ts
@@ -5,12 +5,6 @@ import { Observable } from 'rxjs';
 import { State } from '../../app/state/_state.interfaces';
 import { Plan } from '../../app/models/plan';
 import { getPlans } from '../../app/state/_state.selectors';
-
-import { DetailedPlan, PlanService } from '../../app/services/plan.service';
-import {
-  sortByDatetime,
-  updateDynamicList
-} from '../../app/services/firestore-utils';
 import { WatchPlan } from '../../app/state/plan.action';
 
 @Component({
@@ -18,22 +12,12 @@ import { WatchPlan } from '../../app/state/plan.action';
   templateUrl: 'progress.html'
 })
 export class ProgressPage {
-  plans: DetailedPlan[] = [];
   plans$: Observable<Plan[]>;
 
   constructor(
-    private planService: PlanService,
     private store: Store<State>
   ) {
     this.store.dispatch(new WatchPlan());
     this.plans$ = this.store.select(getPlans);
-    this.plans$.subscribe(plan => console.log(plan));
-  }
-
-  ionViewWillEnter() {
-    this.planService.getPlans().subscribe(plan => {
-      updateDynamicList(this.plans, plan);
-      sortByDatetime({ key: 'modified', objects: this.plans }, 'desc');
-    });
   }
 }
