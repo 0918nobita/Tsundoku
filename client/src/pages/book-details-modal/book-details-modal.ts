8f219d239020b95e1a740b14dbeb4375f997dccf
0918nobita <nobita.0918@gmail.com>
2018-11-23 02:50:22 +0900

    :sparkles: 本の詳細画面で、読み込み中にローディング画面を表示するようにした

--- client/src/pages/book-details-modal/book-details-modal.ts
+++ client/src/pages/book-details-modal/book-details-modal.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { NavParams, ViewController, ToastController } from 'ionic-angular';
+import {
+  NavParams,
+  ViewController,
+  ToastController,
+  Loading,
+  LoadingController
+} from 'ionic-angular';
 
 import { FundamentalModal } from '../fundamental-modal';
 import { Store, select } from '@ngrx/store';
 import { State } from '../../app/state/_state.interfaces';
 import { GetBook } from '../../app/state/book/book.action';
-import { Observable } from 'rxjs';
+import { Observable, from } from 'rxjs';
 import { ResolvedBook } from '../../app/models/resolved-book';
-import { getBookDetail } from '../../app/state/_state.selectors';
+import {
+  getBookDetail,
+  nowLoadingBook
+} from '../../app/state/_state.selectors';
+import { concatMap } from 'rxjs/operators';
 
 @Component({
   selector: 'book-details-modal',
@@ -15,6 +25,7 @@ import { getBookDetail } from '../../app/state/_state.selectors';
 })
 export class BookDetailsModal extends FundamentalModal {
   book$: Observable<ResolvedBook | null>;
+  loader: Loading;
 
   isbn: string;
   title: string;
@@ -23,6 +34,7 @@ export class BookDetailsModal extends FundamentalModal {
 
   constructor(
     private navParams: NavParams,
+    private loadingCtrl: LoadingController,
     protected viewCtrl: ViewController,
     protected toastCtrl: ToastController,
     private store: Store<State>
@@ -30,6 +42,17 @@ export class BookDetailsModal extends FundamentalModal {
     super(viewCtrl, toastCtrl);
     this.isbn = this.navParams.get('isbn');
     this.book$ = this.store.pipe(select(getBookDetail));
+
+    this.loader = this.loadingCtrl.create({
+      content: '読み込み中です…'
+    });
+
+    this.store.pipe(
+      select(nowLoadingBook),
+      concatMap(bool =>
+        from(bool ? this.loader.present() : this.loader.dismiss())
+      )
+    );
 
     this.book$.subscribe(book => {
       if (book === null) return;
