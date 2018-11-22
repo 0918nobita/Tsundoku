ea385dd955a1561c6dc6e143c2a4782c8ff12664
0918nobita <nobita.0918@gmail.com>
2018-11-22 18:37:23 +0900

    :sparkles: BookDetailsModal で、BookService ではなく Store を用いて本の詳細を取得するようにした

--- client/src/pages/book-details-modal/book-details-modal.ts
+++ client/src/pages/book-details-modal/book-details-modal.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
 import { NavParams, ViewController, ToastController } from 'ionic-angular';
 
-import { BookService } from '../../app/services/book.service';
 import { FundamentalModal } from '../fundamental-modal';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
 import { State } from '../../app/state/_state.interfaces';
 import { GetBook } from '../../app/state/book/book.action';
+import { Observable } from 'rxjs';
+import { ResolvedBook } from '../../app/models/resolved-book';
+import { getBookDetail } from '../../app/state/_state.selectors';
 
 @Component({
   selector: 'book-details-modal',
   templateUrl: 'book-details-modal.html'
 })
 export class BookDetailsModal extends FundamentalModal {
+  book$: Observable<ResolvedBook | null>;
+
   isbn: string;
   title: string;
   desc: string;
@@ -21,19 +25,18 @@ export class BookDetailsModal extends FundamentalModal {
     private navParams: NavParams,
     protected viewCtrl: ViewController,
     protected toastCtrl: ToastController,
-    private bookService: BookService,
     private store: Store<State>
   ) {
     super(viewCtrl, toastCtrl);
     this.isbn = this.navParams.get('isbn');
-    this.bookService
-      .getBookByISBN(this.isbn)
-      .then(book => {
-        this.title = book.title;
-        this.desc = book.desc;
-        this.image = book.image;
-      })
-      .catch(err => this.showError(err));
+    this.book$ = this.store.pipe(select(getBookDetail));
+
+    this.book$.subscribe(book => {
+      if (book === null) return;
+      this.title = book.title;
+      this.desc = book.desc;
+      this.image = book.image;
+    });
   }
 
   ionViewWillEnter() {
