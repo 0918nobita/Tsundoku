37bfff12e54cf66cea98f9aae0d6eed74ab78f14
0918nobita <nobita.0918@gmail.com>
2018-11-22 13:19:23 +0900

    :sparkles: FirebaseUI の初期化処理を AuthService ではなく LoginPage で行うようにした

--- client/src/pages/login/login.ts
+++ client/src/pages/login/login.ts
@@ -1,18 +1,44 @@
 import { Component } from '@angular/core';
-
-import { AuthService } from '../../app/services/auth.service';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import * as firebaseui from 'firebaseui';
 
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
 export class LoginPage {
-  constructor(private authService: AuthService) {}
+  ui: firebaseui.auth.AuthUI;
+
+  constructor() {
+    this.ui = new firebaseui.auth.AuthUI(firebase.auth());
+  }
 
   ionViewDidLoad() {
-    this.authService.ui.start(
-      '#firebaseui-auth-container',
-      AuthService.getUIConfig()
-    );
+    this.ui.start('#firebaseui-auth-container', {
+      callbacks: {
+        signInSuccessWithAuthResult: (
+          authResult: firebase.auth.UserCredential
+        ) => {
+          // const user = authResult.user;
+
+          if (
+            (authResult.additionalUserInfo as firebase.auth.AdditionalUserInfo)
+              .isNewUser
+          ) {
+            // Do initialization stuff here (ex. create profile)
+            return false;
+          }
+
+          // Return type determines whether we continue the redirect automatically
+          // or whether we leave that to developer to handle.
+          return false;
+        }
+      },
+      credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+      signInOptions: [firebase.auth.GithubAuthProvider.PROVIDER_ID],
+      tosUrl: 'https://tsundoku.tech',
+      privacyPolicyUrl: 'https://tsundoku.tech'
+    });
   }
 }
