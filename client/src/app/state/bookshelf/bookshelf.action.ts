121ab178a83b8c059eba4d68837cdd903d07c620
0918nobita <nobita.0918@gmail.com>
2018-11-30 17:30:43 +0900

    :construction: WatchBookshelf, WatchBookshelfFail アクションを追加した

--- client/src/app/state/bookshelf/bookshelf.action.ts
+++ client/src/app/state/bookshelf/bookshelf.action.ts
@@ -1,26 +1,34 @@
 import { Action } from '@ngrx/store';
 
-/*
-  本棚に関するアクション
-  - 本棚に手動入力の本を追加する
-  - 本棚に解決済みの本を追加する
-  - 本棚の一括削除 (退会時)
-  */
-
 export enum BookshelfActionTypes {
+  WatchBookshelf = '[Bookshelf] Watch',
+  WatchBookshelfFail = '[Bookshelf] Fail in reading',
   RegisterManuallyAddedBook = '[Bookshelf] Register manually added book',
   RegisterResolvedBook = '[Bookshelf] Register resolved book',
   DeleteBookshelf = '[Bookshelf] Delete'
 }
 
+// 自分の本棚の情報を取得し始める
+export class WatchBookshelf implements Action {
+  readonly type = BookshelfActionTypes.WatchBookshelf;
+}
+
+export class WatchBookshelfFail implements Action {
+  readonly type = BookshelfActionTypes.WatchBookshelfFail;
+  constructor(public payload?: {}) {}
+}
+
+// 手動で本を本棚に追加する
 export class RegisterManuallyAddedBook implements Action {
   readonly type = BookshelfActionTypes.RegisterManuallyAddedBook;
 }
 
+// 解決済みの本を本棚に追加する
 export class RegisterResolvedBook implements Action {
   readonly type = BookshelfActionTypes.RegisterResolvedBook;
 }
 
+// 本棚の一括削除 (退会時)
 export class DeleteBookshelf implements Action {
   readonly type = BookshelfActionTypes.DeleteBookshelf;
 }
