5335d700b0f39f03b189c9ca1e4f017cef3e7ade
0918nobita <nobita.0918@gmail.com>
2018-12-01 17:07:11 +0900

    :construction: getBookshelf, getBooks セレクタを追加した

--- client/src/app/state/_state.selectors.ts
+++ client/src/app/state/_state.selectors.ts
@@ -1,12 +1,24 @@
 import { createSelector } from '@ngrx/store';
 
-import { State, PlanState, AuthState } from './_state.interfaces';
+import {
+  State,
+  PlanState,
+  AuthState,
+  BookshelfState
+} from './_state.interfaces';
 
 export const getAuth = (state: State) => state.auth;
 
 export const getUser = createSelector(
   getAuth,
   (state: AuthState) => state.user
+);
+
+export const getBookshelf = (state: State) => state.bookshelf;
+
+export const getBooks = createSelector(
+  getBookshelf,
+  (state: BookshelfState) => state.books
 );
 
 export const getPlan = (state: State) => state.plan;
