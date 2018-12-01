50197db5233ad0fc22af64b12b82af3505516bff
0918nobita <nobita.0918@gmail.com>
2018-12-01 17:51:00 +0900

    :rewind: 本の詳細ページで、BookService を用いて本の情報を取得するようにした

--- client/src/pages/book-details-modal/book-details-modal.ts
+++ client/src/pages/book-details-modal/book-details-modal.ts
@@ -8,25 +8,13 @@ import {
 } from 'ionic-angular';
 
 import { FundamentalModal } from '../fundamental-modal';
-import { Store, select } from '@ngrx/store';
-import { State } from '../../app/state/_state.interfaces';
-import { GetBook } from '../../app/state/book/book.action';
-import { Observable, from } from 'rxjs';
-import { ResolvedBook } from '../../app/models/resolved-book';
-import {
-  getBookDetail,
-  nowLoadingBook
-} from '../../app/state/_state.selectors';
-import { concatMap } from 'rxjs/operators';
+import { BookService } from '../../app/services/book.service';
 
 @Component({
   selector: 'book-details-modal',
   templateUrl: 'book-details-modal.html'
 })
 export class BookDetailsModal extends FundamentalModal {
-  book$: Observable<ResolvedBook | null>;
-  loader: Loading;
-
   isbn: string;
   title: string;
   desc: string;
@@ -37,35 +25,31 @@ export class BookDetailsModal extends FundamentalModal {
     private loadingCtrl: LoadingController,
     protected viewCtrl: ViewController,
     protected toastCtrl: ToastController,
-    private store: Store<State>
+    private bookService: BookService
   ) {
     super(viewCtrl, toastCtrl);
     this.isbn = this.navParams.get('isbn');
-    this.book$ = this.store.pipe(select(getBookDetail));
 
-    this.loader = this.loadingCtrl.create({
+    const loader = this.loadingCtrl.create({
       content: '読み込み中です…'
     });
 
-    this.store.pipe(
-      select(nowLoadingBook),
-      concatMap(bool =>
-        from(bool ? this.loader.present() : this.loader.dismiss())
-      )
-    );
-
-    this.book$.subscribe(book => {
-      if (book === null) return;
-      this.title = book.title;
-      this.desc = book.desc;
-      this.image = book.image;
-    });
-  }
-
-  ionViewWillEnter() {
-    this.title = '';
-    this.desc = '';
-    this.image = '';
-    this.store.dispatch(new GetBook({ isbn: this.isbn }));
+    loader.present();
+    this.bookService
+      .getBookByISBN(this.isbn)
+      .then(book => {
+        if (book === null) {
+          this.showError('本の情報の取得に失敗しました');
+          return;
+        }
+        this.title = book.title;
+        this.desc = book.desc;
+        this.image = book.image;
+        loader.dismiss();
+      })
+      .catch(err => {
+        loader.dismiss();
+        this.showError(err);
+      });
   }
 }
