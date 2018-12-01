441a644338cbd41e30c328faff27938b9064940d
0918nobita <nobita.0918@gmail.com>
2018-12-01 16:34:11 +0900

    :pencil2: Fix path

--- client/src/app/services/book.service.ts
+++ client/src/app/services/book.service.ts
@@ -4,7 +4,7 @@ import 'firebase/functions';
 import Dexie from 'dexie';
 import axios from 'axios';
 
-import { ResolvedBook } from 'shared/entity';
+import { ResolvedBook } from '../models/resolved-book';
 import { LocalDatabase } from './local-database';
 
 @Injectable({
