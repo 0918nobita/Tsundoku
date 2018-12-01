f9250d5a9548e47ba9dd8ca25d10388418b87542
0918nobita <nobita.0918@gmail.com>
2018-12-01 16:50:26 +0900

    :fire: Delete selectors for BookState

--- client/src/app/state/_state.selectors.ts
+++ client/src/app/state/_state.selectors.ts
@@ -1,24 +1,12 @@
 import { createSelector } from '@ngrx/store';
 
-import { State, PlanState, AuthState, BookState } from './_state.interfaces';
+import { State, PlanState, AuthState } from './_state.interfaces';
 
 export const getAuth = (state: State) => state.auth;
 
 export const getUser = createSelector(
   getAuth,
   (state: AuthState) => state.user
-);
-
-export const getBook = (state: State) => state.book;
-
-export const getBookDetail = createSelector(
-  getBook,
-  (state: BookState) => state.bookDetail
-);
-
-export const nowLoadingBook = createSelector(
-  getBook,
-  (state: BookState) => state.loading
 );
 
 export const getPlan = (state: State) => state.plan;
