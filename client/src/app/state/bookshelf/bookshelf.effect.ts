ced4c207826afee80171cfa685cd0b97fa9e6d4d
0918nobita <nobita.0918@gmail.com>
2018-12-01 16:37:47 +0900

    :sparkles: watchBookshelf エフェクトを実装した

--- client/src/app/state/bookshelf/bookshelf.effect.ts
+++ client/src/app/state/bookshelf/bookshelf.effect.ts
@@ -4,19 +4,41 @@ import { Actions, Effect, ofType } from '@ngrx/effects';
 import {
   WatchBookshelf,
   BookshelfActionTypes,
-  WatchBookshelfFail
+  UpdateBookshelf
 } from './bookshelf.action';
-import { Observable } from 'rxjs';
-import { tap, map } from 'rxjs/operators';
+import { Observable, from } from 'rxjs';
+import { concatMap, map, mergeMap } from 'rxjs/operators';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { mine } from '../../../app/services/firestore-utils';
+import { RegisteredBook } from '../../../app/models/registered-book';
+import { BookService } from '../../../app/services/book.service';
 
 @Injectable()
 export class BookshelfEffects {
-  constructor(private actions$: Actions) {}
+  constructor(
+    private actions$: Actions,
+    private afFirestore: AngularFirestore,
+    private bookService: BookService
+  ) {}
 
   @Effect()
   watchBookshelf: Observable<Action> = this.actions$.pipe(
     ofType<WatchBookshelf>(BookshelfActionTypes.WatchBookshelf),
-    tap(x => console.log(`watchBookshelf: ${x}`)),
-    map(() => new WatchBookshelfFail())
+    concatMap(() => {
+      const nextRegisteredBooks = (registeredBooks: RegisteredBook[]) =>
+        from(registeredBooks).pipe(
+          mergeMap(registeredBook =>
+            from(this.bookService.embedBookDetails(registeredBook))
+          )
+        );
+
+      return this.afFirestore
+        .collection<RegisteredBook>('bookshelf', mine)
+        .valueChanges()
+        .pipe(
+          mergeMap(nextRegisteredBooks),
+          map(book => new UpdateBookshelf({ book }))
+        );
+    })
   );
 }
