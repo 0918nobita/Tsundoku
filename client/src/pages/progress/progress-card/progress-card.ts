bda56d40e28ee6e18cf75e4036dcb09d3969be96
0918nobita <nobita.0918@gmail.com>
2018-12-13 00:13:34 +0900

    :sparkles: ProgressCard コンポーネントにサムネイルと進捗率の図を表示するようにした

--- client/src/pages/progress/progress-card/progress-card.ts
+++ client/src/pages/progress/progress-card/progress-card.ts
@@ -1,4 +1,5 @@
 import { Component, Input, ViewChild, ElementRef } from '@angular/core';
+import { Chart } from 'chart.js';
 
 import { Plan } from '../../../app/models/plan';
 
@@ -8,4 +9,40 @@ import { Plan } from '../../../app/models/plan';
 })
 export class ProgressCard {
   @Input() plan: Plan;
+  @ViewChild('graph') graph: ElementRef;
+
+  ngAfterViewInit() {
+    const context = this.graph.nativeElement.getContext('2d');
+    new Chart(context, {
+      type: 'doughnut',
+      data: {
+        labels: ['読んだ', '読んでない'],
+        datasets: [
+          {
+            label: 'progress',
+            data: [
+              this.plan.progress,
+              this.plan.book.pageCount - this.plan.progress
+            ],
+            backgroundColor: ['#36A2EB', '#CECECE'],
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        title: {
+          text: `${percentage(this.plan.progress, this.plan.book.pageCount)} %`,
+          display: true
+        },
+        legend: { display: false },
+        responsive: true,
+        maintainAspectRatio: false,
+        animation: { animateScale: true }
+      }
+    });
+
+    function percentage(samples: number, total: number) {
+      return Math.round((samples / total) * 100 * 10) / 10;
+    }
+  }
 }
