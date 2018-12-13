c576c5c12745fdcfdb86e7c725539a4c7a543633
0918nobita <nobita.0918@gmail.com>
2018-12-14 02:05:50 +0900

    :sparkles: 進捗ページで「読書計画を追加する」ボタンを押すと PlanAdditionModal が開くようにした

--- client/src/pages/progress/progress.ts
+++ client/src/pages/progress/progress.ts
@@ -6,6 +6,8 @@ import { State } from '../../app/state/_state.interfaces';
 import { Plan } from '../../app/models/plan';
 import { getPlans } from '../../app/state/_state.selectors';
 import { WatchPlan } from '../../app/state/plan/plan.action';
+import { ModalController } from 'ionic-angular';
+import { PlanAdditionModal } from './plan-addition-modal/plan-addition-modal';
 
 @Component({
   selector: 'page-about',
@@ -14,8 +16,12 @@ import { WatchPlan } from '../../app/state/plan/plan.action';
 export class ProgressPage {
   plans$: Observable<Plan[]>;
 
-  constructor(private store: Store<State>) {
+  constructor(private store: Store<State>, private modalCtrl: ModalController) {
     this.store.dispatch(new WatchPlan());
     this.plans$ = this.store.pipe(select(getPlans));
+  }
+
+  openPlanAdditionModal() {
+    this.modalCtrl.create(PlanAdditionModal).present();
   }
 }
