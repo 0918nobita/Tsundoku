ed42123ee2975b342b313a15ce441ac8d975816f
0918nobita <nobita.0918@gmail.com>
2018-12-16 00:40:53 +0900

    :building_construction: Functions 側の deleteSkill 関数の引数の形式を変更した

--- functions/src/skill.ts
+++ functions/src/skill.ts
@@ -15,11 +15,23 @@ export const _createSkill = (db: FirebaseFirestore.Firestore) =>
   };
 
 export const _deleteSkill = (db: FirebaseFirestore.Firestore) =>
-  async (id: string) => {
+  async (args: {
+    isbn: string;
+    content: string;
+    uid: string;
+  }) => {
     try {
-      await db.collection('skill').doc(id).delete();
+      const docs = (await db.collection('skills').where('uid', '==', args.uid).get()).docs;
+      for (const doc of docs) {
+        const data = doc.data();
+        if (data.isbn !== void 0 && data.isbn === args.isbn &&
+            data.content !== void 0 && data.content === args.content) {
+          await db.collection('skills').doc(doc.id).delete();
+        }
+      }
       return true;
-    } catch {
+    } catch (e) {
+      console.error(e);
       return false;
     }
   };
