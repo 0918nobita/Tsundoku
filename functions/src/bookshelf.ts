0bf965c52d9ad98e6edf39e80ead0cac1b76fbb5
0918nobita <nobita.0918@gmail.com>
2018-12-15 17:34:20 +0900

    :sparkles: Functions 側に unregisterBook 関数を追加した

--- functions/src/bookshelf.ts
+++ functions/src/bookshelf.ts
@@ -11,3 +11,25 @@ export const _registerBook = (db: FirebaseFirestore.Firestore) => async (args: {
     modified: date
   })).id;
 };
+
+export const _unregisterBook = (db: FirebaseFirestore.Firestore) => async (args: {
+  isbn: string;
+  uid: string;
+}): Promise<boolean> => {
+  try {
+    const querySnapshots = (await db.collection('bookshelf').where('uid', '==', args.uid).get()).docs;
+
+    for (const snapshot of querySnapshots) {
+      const data = snapshot.data();
+      if (data.isbn !== void 0 && data.isbn === args.isbn) {
+        await db.collection('bookshelf').doc(snapshot.id).delete();
+        return true;
+      }
+    }
+
+    return false;
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
+};
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,7 @@ import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
 
 import { _searchBooksByISBN, _postResolvedBook } from "./book";
-import { _registerBook } from "./bookshelf";
+import { _registerBook, _unregisterBook } from "./bookshelf";
 import { _createSkill, _deleteSkill } from "./skill";
 
 admin.initializeApp(functions.config().firebase);
@@ -13,6 +13,7 @@ export const searchBooksByISBN = functions.https.onCall(_searchBooksByISBN(db));
 export const postResolvedBook = functions.https.onCall(_postResolvedBook(db));
 
 export const registerBook = functions.https.onCall(_registerBook(db));
+export const unregisterBook = functions.https.onCall(_unregisterBook(db));
 
 export const createSkill = functions.https.onCall(_createSkill(db));
 export const deleteSkill = functions.https.onCall(_deleteSkill(db));
