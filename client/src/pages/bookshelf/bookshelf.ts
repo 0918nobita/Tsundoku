04ca8f8c43ceca252133ca3a1d2228d286df7aa3
0918nobita <nobita.0918@gmail.com>
2018-12-16 12:31:39 +0900

    :fire: 検索欄とフラグメントを削除した

--- client/src/pages/bookshelf/bookshelf.ts
+++ client/src/pages/bookshelf/bookshelf.ts
@@ -1,13 +1,12 @@
-import { Component, ViewChild, ElementRef, HostListener } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Store, select } from '@ngrx/store';
-import { Observable, BehaviorSubject } from 'rxjs';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { RegisteredBook } from '../../app/models/registered-book';
 import { State } from '../../app/state/_state.interfaces';
 import { WatchBookshelf } from '../../app/state/bookshelf/bookshelf.action';
 import { getBooks } from '../../app/state/_state.selectors';
-import { Platform, Events } from 'ionic-angular';
 
 enum Fragment {
   Library,
@@ -26,19 +25,8 @@ export class BookshelfPage {
   text = '';
   @ViewChild('libraryFragment') libraryFragment;
   @ViewChild('searchFragment') searchFragment;
-  nativeElement: HTMLElement;
-  mdSearchIcon: HTMLElement;
-  mdCancelButton: HTMLElement;
-  iosCancelButton: HTMLElement;
-  currentTabs: BehaviorSubject<number>;
-  currentSearchMethod: BehaviorSubject<string>;
 
-  constructor(
-    private store: Store<State>,
-    private platform: Platform,
-    private events: Events,
-    private el: ElementRef
-  ) {}
+  constructor(private store: Store<State>) {}
 
   ionViewDidLoad() {
     window.addEventListener('resize', () => this.adjustThumbnails());
@@ -53,39 +41,6 @@ export class BookshelfPage {
         return books;
       })
     );
-
-    this.mdSearchIcon = this.querySelector('.searchbar-search-icon');
-    this.mdCancelButton = this.querySelector('.searchbar-md-cancel');
-    this.mdCancelButton.addEventListener('click', () => {
-      this.onCancelSearch();
-      this.mdCancelButton.style.display = 'none';
-      this.mdSearchIcon.style.display = 'block';
-    });
-
-    this.iosCancelButton = this.querySelector('.searchbar-ios-cancel');
-    this.iosCancelButton.addEventListener('click', () => {
-      this.onCancelSearch();
-      this.iosCancelButton.style.display = 'none';
-    });
-
-    this.currentTabs = new BehaviorSubject(0);
-    this.events.subscribe('tabs:changed', x => this.currentTabs.next(x));
-
-    this.currentSearchMethod = new BehaviorSubject('isbn');
-    this.events.subscribe('searchMethod:changed', method => {
-      this.currentSearchMethod.next(method);
-      this.updatePlaceholder();
-    });
-  }
-
-  switchFragment(to = Fragment.Search) {
-    this.libraryFragment.nativeElement.style.display =
-      to === Fragment.Search ? 'none' : 'block';
-
-    this.searchFragment.nativeElement.style.display =
-      to === Fragment.Search ? 'block' : 'none';
-
-    this.fragment = to;
   }
 
   adjustThumbnails() {
@@ -123,55 +78,5 @@ export class BookshelfPage {
     } else if (diff < this.additions.length) {
       this.additions.splice(0, this.additions.length - diff);
     }
-  }
-
-  onFocus() {
-    const platforms = this.platform.platforms();
-
-    if (platforms.indexOf('ios') !== -1) {
-      this.iosCancelButton.style.display = 'block';
-    } else {
-      this.mdSearchIcon.style.display = 'none';
-      this.mdCancelButton.style.display = 'block';
-    }
-
-    this.switchFragment();
-    this.updatePlaceholder();
-  }
-
-  updateText() {
-    console.log(`Update: ${this.text}`);
-  }
-
-  onCancelSearch() {
-    this.querySelector('.searchbar-input').setAttribute(
-      'placeholder',
-      '追加する本を検索'
-    );
-    (document.activeElement as HTMLElement).blur();
-    this.switchFragment(Fragment.Library);
-  }
-
-  @HostListener('document:keyup.esc')
-  esc() {
-    if (this.currentTabs.getValue() !== 0) return;
-    if (this.fragment !== Fragment.Search) return;
-    (this.platform.platforms().indexOf('ios') !== -1
-      ? this.iosCancelButton
-      : this.mdCancelButton
-    ).click();
-  }
-
-  private updatePlaceholder() {
-    this.querySelector('.searchbar-input').setAttribute(
-      'placeholder',
-      this.currentSearchMethod.getValue() === 'isbn'
-        ? 'ISBN を入力 (13 桁)'
-        : 'スキルをフリーワードで検索'
-    );
-  }
-
-  private querySelector(selector: string): HTMLElement {
-    return this.el.nativeElement.querySelector(selector);
   }
 }
