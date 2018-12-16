c6a849156f466de73d5acecc41670a18ed840086
0918nobita <nobita.0918@gmail.com>
2018-12-16 15:41:44 +0900

    :sparkles: SearchModal で、instantsearch.js を用いてスキルを検索できるようにした

--- client/src/pages/bookshelf/search-modal/search-modal.ts
+++ client/src/pages/bookshelf/search-modal/search-modal.ts
@@ -1,16 +1,46 @@
 import { FundamentalModal } from '../../../pages/fundamental-modal';
 import { Component } from '@angular/core';
 import { ViewController, ToastController } from 'ionic-angular';
+import instantsearch from 'instantsearch.js/es';
+import { searchBox, analytics } from 'instantsearch.js/es/widgets';
+import { algoliaConfig } from '../../../app/config';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'search-modal',
   templateUrl: 'search-modal.html'
 })
 export class SearchModal extends FundamentalModal {
+  search;
+  hits$: Subject<{ isbn: string; Content: string }[]>;
+
   constructor(
     protected viewCtrl: ViewController,
     protected toastCtrl: ToastController
   ) {
     super(viewCtrl, toastCtrl);
+    this.hits$ = new Subject();
+  }
+
+  ngOnInit() {
+    this.search = instantsearch(algoliaConfig);
+
+    this.search.addWidget(
+      searchBox({
+        container: '#search-box',
+        autofocus: false,
+        placeholder: 'ISBN 13桁で本を検索、またはスキルを検索する'
+      })
+    );
+
+    this.search.addWidget(
+      analytics({
+        pushFunction: (query, state, results) => {
+          this.hits$.next(results.hits);
+        }
+      })
+    );
+
+    this.search.start();
   }
 }
