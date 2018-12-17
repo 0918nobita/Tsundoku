d554b24de2f876ef48cea732a6fc61c65d822bab
0918nobita <nobita.0918@gmail.com>
2018-12-17 17:33:12 +0900

    :construction: UpdateSkill アクションの Reducer を追加した

--- client/src/app/state/skill/skill.reducer.ts
+++ client/src/app/state/skill/skill.reducer.ts
@@ -2,13 +2,20 @@ import { Action } from '@ngrx/store';
 
 import { initialSkillState } from '../_state.inits';
 import { SkillState } from '../_state.interfaces';
-import { SkillActionTypes } from './skill.action';
+import { SkillActionTypes, UpdateSkill } from './skill.action';
+import { sortByDatetime } from '../../../app/services/firestore-utils';
 
 export function reducer(state = initialSkillState, action: Action): SkillState {
   switch (action.type) {
     case SkillActionTypes.WatchSkill:
       return state;
 
+    case SkillActionTypes.UpdateSkill:
+      const skills = (action as UpdateSkill).payload;
+      sortByDatetime({ key: 'created', objects: skills }, 'desc');
+      return Object.assign({}, { ...state, skills });
+
+    case SkillActionTypes.WatchSkillFail:
     case SkillActionTypes.CreateSkill:
     case SkillActionTypes.CreateSkillSuccess:
     case SkillActionTypes.CreateSkillFail:
