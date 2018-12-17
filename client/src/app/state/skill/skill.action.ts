eede3ef1b3c245fcf4a82bebb7b91cf2062a23d9
0918nobita <nobita.0918@gmail.com>
2018-12-17 17:30:42 +0900

    :construction: WatchSkill アクションを追加した

--- client/src/app/state/skill/skill.action.ts
+++ client/src/app/state/skill/skill.action.ts
@@ -2,6 +2,7 @@ import { Action } from '@ngrx/store';
 import { Skill } from '../../models/skill';
 
 export enum SkillActionTypes {
+  WatchSkill = '[Skill] Watch',
   CreateSkill = '[Skill] Create',
   CreateSkillSuccess = '[Skill] Succeed in creating',
   CreateSkillFail = '[Skill] Fail in creating',
@@ -10,34 +11,32 @@ export enum SkillActionTypes {
   DeleteSkillFail = '[Skill] Fail in deleting'
 }
 
-// スキルを作成する
+export class WatchSkill implements Action {
+  readonly type = SkillActionTypes.WatchSkill;
+}
+
 export class CreateSkill implements Action {
   readonly type = SkillActionTypes.CreateSkill;
   constructor(public payload: { skill: Skill }) {}
 }
 
-// スキルの作成に成功した
 export class CreateSkillSuccess implements Action {
   readonly type = SkillActionTypes.CreateSkillSuccess;
 }
 
-// スキルの作成に成功した
 export class CreateSkillFail implements Action {
   readonly type = SkillActionTypes.CreateSkillFail;
 }
 
-// スキルを削除する
 export class DeleteSkill implements Action {
   readonly type = SkillActionTypes.DeleteSkill;
   constructor(public payload: { id: string }) {}
 }
 
-// スキルの削除に成功した
 export class DeleteSkillSuccess implements Action {
   readonly type = SkillActionTypes.DeleteSkillSuccess;
 }
 
-// スキルの削除に失敗した
 export class DeleteSkillFail implements Action {
   readonly type = SkillActionTypes.DeleteSkillFail;
 }
--- client/src/app/state/skill/skill.reducer.ts
+++ client/src/app/state/skill/skill.reducer.ts
@@ -6,8 +6,15 @@ import { SkillActionTypes } from './skill.action';
 
 export function reducer(state = initialSkillState, action: Action): SkillState {
   switch (action.type) {
+    case SkillActionTypes.WatchSkill:
+      return state;
+
     case SkillActionTypes.CreateSkill:
+    case SkillActionTypes.CreateSkillSuccess:
+    case SkillActionTypes.CreateSkillFail:
     case SkillActionTypes.DeleteSkill:
+    case SkillActionTypes.DeleteSkillSuccess:
+    case SkillActionTypes.DeleteSkillFail:
     default:
       return state;
   }
