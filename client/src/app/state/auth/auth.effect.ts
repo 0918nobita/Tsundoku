8501eb432208ee375b9690dba2992e7296f2dd77
0918nobita <nobita.0918@gmail.com>
2018-12-17 21:43:25 +0900

    :sparkles: AuthEffects に forAuthenticated メソッドを追加した
    ログイン中のユーザの uid を Observable で流し、未ログインなどの理由で失敗した場合はエラーを吐く

--- client/src/app/state/auth/auth.effect.ts
+++ client/src/app/state/auth/auth.effect.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Action } from '@ngrx/store';
+import { Action, Store, select } from '@ngrx/store';
 import { Actions, Effect, ofType } from '@ngrx/effects';
 import { Observable, of, BehaviorSubject } from 'rxjs';
-import { concatMap, catchError, mergeMap } from 'rxjs/operators';
+import { concatMap, catchError, mergeMap, map } from 'rxjs/operators';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
@@ -13,12 +13,18 @@ import {
   SignInSuccess
 } from './auth.action';
 import { LocalDatabase } from '../../services/local-database';
+import { AuthState, State } from '../_state.interfaces';
+import { getUser } from '../_state.selectors';
 
 @Injectable()
 export class AuthEffects {
   signInSubject: BehaviorSubject<firebase.User | null | undefined>;
 
-  constructor(private actions$: Actions, private localDB: LocalDatabase) {
+  constructor(
+    private actions$: Actions,
+    private localDB: LocalDatabase,
+    private store: Store<State>
+  ) {
     this.signInSubject = new BehaviorSubject(void 0);
     firebase.auth().onAuthStateChanged(this.signInSubject);
   }
@@ -42,5 +48,15 @@ export class AuthEffects {
       )
     ),
     catchError(error => of(new SignInFail({ error })))
+  );
+
+  forAuthenticated = this.store.pipe(
+    select(getUser),
+    map(me => {
+      if (me === null || me === void 0) {
+        throw new Error('ユーザー情報の取得に失敗しました');
+      }
+      return me.uid;
+    })
   );
 }
