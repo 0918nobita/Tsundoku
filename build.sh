69d2fac7b8e251271a1e119013fb63767c94e693
0918nobita <nobita.0918@gmail.com>
2018-12-18 11:51:44 +0900

    :rotating_light: Remove linter warnings

--- build.sh
+++ build.sh
@@ -3,17 +3,17 @@ npm i
 cd ../client
 {
   echo "export const firebaseConfig = {"
-  echo "  apiKey: \"$API_KEY\","
-  echo "  authDomain: \"$AUTH_DOMAIN\","
-  echo "  databaseURL: \"$DATABASE_URL\","
-  echo "  messagingSenderId: \"$MESSAGING_SENDER_ID\","
-  echo "  projectId: \"$PROJECT_ID\","
-  echo "  storageBucket: \"$STORAGE_BUCKET\""
+  echo "  apiKey: '$API_KEY',"
+  echo "  authDomain: '$AUTH_DOMAIN',"
+  echo "  databaseURL: '$DATABASE_URL',"
+  echo "  messagingSenderId: '$MESSAGING_SENDER_ID',"
+  echo "  projectId: '$PROJECT_ID',"
+  echo "  storageBucket: '$STORAGE_BUCKET'"
   echo "};"
   echo "export const algoliaConfig = {"
-  echo "  appId: \"$ALGOLIA_APP_ID\","
-  echo "  apiKey: \"$ALGOLIA_API_KEY\","
-  echo "  indexName: \"$ALGOLIA_INDEX_NAME\""
+  echo "  appId: '$ALGOLIA_APP_ID',"
+  echo "  apiKey: '$ALGOLIA_API_KEY',"
+  echo "  indexName: '$ALGOLIA_INDEX_NAME'"
   echo "}"
 } >> src/app/config.ts
 cat src/app/config.ts
