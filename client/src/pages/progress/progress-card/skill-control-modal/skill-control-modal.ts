301160cd88c3faeafe9465eaa0a0ccd55f32aff6
0918nobita <nobita.0918@gmail.com>
2018-12-19 19:49:13 +0900

    :construction: SkillControlModal クラスに FundamentalModal クラスを継承させた

--- client/src/pages/progress/progress-card/skill-control-modal/skill-control-modal.ts
+++ client/src/pages/progress/progress-card/skill-control-modal/skill-control-modal.ts
@@ -1,6 +1,16 @@
 import { Component } from '@angular/core';
+import { NavParams, ViewController, ToastController } from 'ionic-angular';
+import { FundamentalModal } from '../../../../pages/fundamental-modal';
 
 @Component({
   templateUrl: 'skill-control-modal.html'
 })
-export class SkillControlModal {}
+export class SkillControlModal extends FundamentalModal {
+  constructor(
+    protected viewCtrl: ViewController,
+    protected toastCtrl: ToastController,
+    private navParams: NavParams
+  ) {
+    super(viewCtrl, toastCtrl);
+  }
+}
