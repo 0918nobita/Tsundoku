6a16fa3c0dc206194712f08a0f604774aa8c83a2
0918nobita <nobita.0918@gmail.com>
2018-12-21 03:53:28 +0900

    :sparkles: SkillControlModal でスキル一覧の表示/追加/削除ができるようにした

--- client/src/pages/progress/progress-card/progress-card.ts
+++ client/src/pages/progress/progress-card/progress-card.ts
@@ -1,29 +1,9 @@
-import {
-  Component,
-  Input,
-  ViewChild,
-  ElementRef,
-  HostListener
-} from '@angular/core';
+import { Component, Input, ViewChild, ElementRef } from '@angular/core';
 import { Chart } from 'chart.js';
 
 import { Plan } from '../../../app/models/plan';
-import { Observable } from 'rxjs';
-import { Skill } from '../../../app/models/skill';
-import { SkillService } from '../../../app/services/skill.service';
-import {
-  AlertController,
-  LoadingController,
-  ModalController
-} from 'ionic-angular';
-import { Store, select } from '@ngrx/store';
-import { State } from '../../../app/state/_state.interfaces';
-import {
-  CreateSkill,
-  DeleteSkill
-} from '../../../app/state/skill/skill.action';
-import { getSkillProgress } from '../../../app/state/_state.selectors';
 import { SkillControlModal } from './skill-control-modal/skill-control-modal';
+import { ModalController } from 'ionic-angular';
 
 @Component({
   selector: 'progress-card',
@@ -32,20 +12,10 @@ import { SkillControlModal } from './skill-control-modal/skill-control-modal';
 export class ProgressCard {
   @Input() plan: Plan;
   @ViewChild('graph') graph: ElementRef;
-  skills$: Observable<Skill[]>;
-  conversion = false;
 
-  constructor(
-    private skillService: SkillService,
-    private alertCtrl: AlertController,
-    private loadingCtrl: LoadingController,
-    private modalCtrl: ModalController,
-    private store: Store<State>
-  ) {}
+  constructor(private modalCtrl: ModalController) {}
 
   ngAfterViewInit() {
-    this.skills$ = this.skillService.getSkills(this.plan.uid, this.plan.isbn);
-
     const context = this.graph.nativeElement.getContext('2d');
     new Chart(context, {
       type: 'doughnut',
@@ -79,62 +49,7 @@ export class ProgressCard {
     }
   }
 
-  createSkillListener() {
-    this.alertCtrl
-      .create({
-        title: 'スキルの追加',
-        inputs: [
-          {
-            name: 'content',
-            placeholder: 'スキルの内容 (必須)'
-          }
-        ],
-        buttons: [
-          {
-            text: 'キャンセル',
-            role: 'cancel'
-          },
-          {
-            text: '追加',
-            handler: data => {
-              if (this.conversion) {
-                this.conversion = false;
-                return false;
-              }
-              this.createSkill(data.content);
-            }
-          }
-        ]
-      })
-      .present();
-  }
-
-  openSkillControlModal() {
-    this.modalCtrl.create(SkillControlModal).present();
-  }
-
-  async createSkill(content: string) {
-    this.store.dispatch(new CreateSkill(this.plan.isbn, content));
-    this.loading('追加処理中です…');
-  }
-
-  async deleteSkill(skill: Skill) {
-    this.store.dispatch(new DeleteSkill(skill));
-    this.loading('削除処理中です…');
-  }
-
-  private loading(content: string) {
-    const loader = this.loadingCtrl.create({ content });
-    loader.present();
-    this.store.pipe(select(getSkillProgress)).subscribe(progress => {
-      if (progress === 'complete') loader.dismiss();
-    });
-  }
-
-  @HostListener('document:keydown', ['$event'])
-  onKeydown(event: KeyboardEvent) {
-    /* tslint:disable:deprecation */
-    if (event.keyCode === 229) this.conversion = true;
-    else if (event.keyCode === 13) this.conversion = false;
+  openSkillControlModal(plan: Plan) {
+    this.modalCtrl.create(SkillControlModal, { plan }).present();
   }
 }
