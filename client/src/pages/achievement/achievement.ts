84ca525a02281c9a75967bedc18f66bf133073dd
0918nobita <nobita.0918@gmail.com>
2018-12-21 18:12:07 +0900

    :construction: スキル数の推移を表すグラフのサンプルを追加した

--- client/src/pages/achievement/achievement.ts
+++ client/src/pages/achievement/achievement.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import moment from 'moment';
+import { Chart } from 'chart.js';
 
 import { Skill } from '../../app/models/skill';
 import { Store, select } from '@ngrx/store';
@@ -12,9 +13,29 @@ import { WatchSkill } from '../../app/state/skill/skill.action';
   templateUrl: 'achievement.html'
 })
 export class AchievementPage {
+  @ViewChild('graph') graph: ElementRef;
   skills$: Observable<Skill[]>;
 
   constructor(private store: Store<State>) {}
+
+  ngAfterViewInit() {
+    const context = this.graph.nativeElement.getContext('2d');
+    new Chart(context, {
+      type: 'line',
+      data: {
+        labels: ['a', 'b', 'c', 'd'],
+        datasets: [
+          {
+            label: 'スキル数',
+            data: [0, 20, 40, 80],
+            fill: false,
+            borderColor: '#488aff'
+          }
+        ]
+      },
+      options: {}
+    });
+  }
 
   ionViewDidLoad() {
     this.store.dispatch(new WatchSkill());
