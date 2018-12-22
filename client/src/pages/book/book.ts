a68cc6e38149ea582a61b911dbbcb151610b7761
0918nobita <nobita.0918@gmail.com>
2018-12-22 15:55:10 +0900

    :sparkles: book-widget で、サムネイルが取得できないときにタイトルをもとにして自動生成するようにした

--- client/src/pages/book/book.ts
+++ client/src/pages/book/book.ts
@@ -1,17 +1,42 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  Input,
+  ViewChildren,
+  QueryList,
+  ElementRef
+} from '@angular/core';
 import { ModalController } from 'ionic-angular';
 import { BookDetailsModal } from '../book-details-modal/book-details-modal';
-import { ResolvedBook } from 'shared/entity';
+import { ResolvedBook } from '../../app/models/resolved-book';
 
 @Component({
   selector: 'book-widget',
   templateUrl: 'book.html'
 })
 export class Book {
+  @ViewChildren('thumbnail') thumbnail: QueryList<ElementRef>;
   @Input()
   book: ResolvedBook;
 
   constructor(private modalCtrl: ModalController) {}
+
+  ngAfterViewInit() {
+    const children = this.thumbnail.toArray();
+    if (children.length > 0) {
+      const ctx: CanvasRenderingContext2D = this.thumbnail.first.nativeElement.getContext(
+        '2d'
+      );
+      ctx.fillStyle = '#EEEEEE';
+      ctx.fillRect(0, 0, 128, 164);
+      ctx.fillStyle = '#222222';
+      ctx.beginPath();
+      ctx.font = 'bold 15px Arial, meiryo, sans-serif';
+      const lines = this.book.title.match(/[\s\S]{1,8}/g) || [];
+      for (let i = 0; i < lines.length; i++) {
+        ctx.fillText(lines[i], 10, 10 + (i + 1) * 15);
+      }
+    }
+  }
 
   showDetails() {
     this.modalCtrl.create(BookDetailsModal, { isbn: this.book.isbn }).present();
