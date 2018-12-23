8151d726b4dec44a7ed3b2cac9893af08aa5bd35
0918nobita <nobita.0918@gmail.com>
2018-12-23 21:19:45 +0900

    :construction: SignOut/Success/Fail アクションに対する Reducer を追加した

--- client/src/app/state/auth/auth.reducer.ts
+++ client/src/app/state/auth/auth.reducer.ts
@@ -6,14 +6,16 @@ import { AuthActionTypes, SignInSuccess } from './auth.action';
 
 export function reducer(state = initialAuthState, action: Action): AuthState {
   switch (action.type) {
-    case AuthActionTypes.SignIn:
-      return state;
-
     case AuthActionTypes.SignInSuccess:
       const user = (action as SignInSuccess).payload.user;
       return Object.assign({}, { ...state, user });
 
+    case AuthActionTypes.SignOutSuccess:
+      return Object.assign({}, { ...state, user: null });
+
+    case AuthActionTypes.SignIn:
     case AuthActionTypes.SignInFail:
+    case AuthActionTypes.SignOutFail:
     default:
       return state;
   }
