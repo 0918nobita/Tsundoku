bc3c66960c46b9ed1f8559911c10f270086c8188
0918nobita <nobita.0918@gmail.com>
2018-12-23 21:15:39 +0900

    :construction: 本当に退会するかどうかを確認するアラートを追加した

--- client/src/pages/settings-button/settings-modal/settings-modal.ts
+++ client/src/pages/settings-button/settings-modal/settings-modal.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
-import { ViewController, ToastController } from 'ionic-angular';
+import {
+  ViewController,
+  ToastController,
+  AlertController
+} from 'ionic-angular';
 import { FundamentalModal } from '../../fundamental-modal';
+import { State } from '../../../app/state/_state.interfaces';
+import { Store } from '@ngrx/store';
+import { SignOut } from '../../../app/state/auth/auth.action';
 
 @Component({
   templateUrl: 'settings-modal.html'
@@ -8,8 +15,30 @@ import { FundamentalModal } from '../../fundamental-modal';
 export class SettingsModal extends FundamentalModal {
   constructor(
     protected viewCtrl: ViewController,
-    protected toastCtrl: ToastController
+    protected toastCtrl: ToastController,
+    private alertCtrl: AlertController,
+    private store: Store<State>
   ) {
     super(viewCtrl, toastCtrl);
+  }
+  withdraw() {
+    this.alertCtrl
+      .create({
+        title: '本当に退会しますか？',
+        message: 'この操作はやり直せません。',
+        buttons: [
+          {
+            text: '退会する',
+            handler: () => {
+              console.log('退会処理');
+            }
+          },
+          {
+            text: 'キャンセル',
+            role: 'cancel'
+          }
+        ]
+      })
+      .present();
   }
 }
