b4389e99f75677e6046a0dc26957cde1b40bc1ac
0918nobita <nobita.0918@gmail.com>
2018-12-24 13:02:52 +0900

    :heavy_minus_sign: Uninstall @sgbj/angular-gist & postscribe

--- client/src/pages/login/login.module.ts
+++ client/src/pages/login/login.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { LoginPage } from './login';
-import { GistModule } from '@sgbj/angular-gist';
 
 @NgModule({
   declarations: [LoginPage],
-  imports: [IonicPageModule.forChild(LoginPage), GistModule]
+  imports: [IonicPageModule.forChild(LoginPage)]
 })
 export class LoginPageModule {}
