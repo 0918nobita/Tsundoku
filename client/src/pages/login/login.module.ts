b90a5449c93ee95b14fd8da895654b61a8f01765
0918nobita <nobita.0918@gmail.com>
2018-12-24 14:25:21 +0900

    :sparkles: LoginPage で gist を埋め込み表示するようにした

--- client/src/pages/login/login.module.ts
+++ client/src/pages/login/login.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { LoginPage } from './login';
 
+import { NgxGistModule } from 'ngx-gist/dist/ngx-gist.module';
+
 @NgModule({
   declarations: [LoginPage],
-  imports: [IonicPageModule.forChild(LoginPage)]
+  imports: [IonicPageModule.forChild(LoginPage), NgxGistModule]
 })
 export class LoginPageModule {}
