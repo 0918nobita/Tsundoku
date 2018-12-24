38e5f1f2acce4d4e219b16450b5e652517191b86
0918nobita <nobita.0918@gmail.com>
2018-12-24 15:21:27 +0900

    :sparkles: 達成ページで、月毎のスキル数の推移をグラフ化して表示するようにした

--- client/src/pages/achievement/achievement.ts
+++ client/src/pages/achievement/achievement.ts
@@ -1,12 +1,13 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import moment from 'moment';
 import { Chart } from 'chart.js';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Store, select } from '@ngrx/store';
 
 import { Skill } from '../../app/models/skill';
-import { Store, select } from '@ngrx/store';
 import { getSkills } from '../../app/state/_state.selectors';
 import { State } from '../../app/state/_state.interfaces';
-import { Observable } from 'rxjs';
 import { WatchSkill } from '../../app/state/skill/skill.action';
 
 @Component({
@@ -15,19 +16,70 @@ import { WatchSkill } from '../../app/state/skill/skill.action';
 export class AchievementPage {
   @ViewChild('graph') graph: ElementRef;
   skills$: Observable<Skill[]>;
+  labels: string[] = [];
+  data: number[] = [];
 
   constructor(private store: Store<State>) {}
 
   ngAfterViewInit() {
+    this.updateChart();
+  }
+
+  ionViewDidLoad() {
+    this.store.dispatch(new WatchSkill());
+    this.skills$ = this.store.pipe(
+      select(getSkills),
+      map(skills => {
+        this.labels = [];
+        this.data = [];
+        let sum = 0;
+        const dateWithSum = {};
+        const labelsOrder: string[] = [];
+
+        for (let i = skills.length - 1; i >= 0; i--) {
+          sum++;
+          const date = skills[i].created.toDate();
+          const year = date.getFullYear();
+          const month = date.getMonth() + 1;
+          const key = `${year}/${month}`;
+          dateWithSum[key] = sum;
+
+          if (
+            labelsOrder.length === 0 ||
+            labelsOrder[labelsOrder.length - 1] !== key
+          ) {
+            labelsOrder.push(key);
+          }
+        }
+
+        this.data.push(0);
+        this.labels.push('');
+        for (const label of labelsOrder) {
+          this.data.push(dateWithSum[label]);
+          this.labels.push(label);
+        }
+
+        this.updateChart();
+        return skills;
+      })
+    );
+  }
+
+  differenceTime(time: Date) {
+    return moment(time).fromNow();
+  }
+
+  updateChart() {
+    if (this.labels.length === 0) return;
     const context = this.graph.nativeElement.getContext('2d');
     new Chart(context, {
       type: 'line',
       data: {
-        labels: ['a', 'b', 'c', 'd'],
+        labels: this.labels,
         datasets: [
           {
             label: 'スキル数',
-            data: [0, 20, 40, 80],
+            data: this.data,
             fill: false,
             borderColor: '#488aff'
           }
@@ -35,14 +87,5 @@ export class AchievementPage {
       },
       options: {}
     });
-  }
-
-  ionViewDidLoad() {
-    this.store.dispatch(new WatchSkill());
-    this.skills$ = this.store.pipe(select(getSkills));
-  }
-
-  differenceTime(time: Date) {
-    return moment(time).fromNow();
   }
 }
