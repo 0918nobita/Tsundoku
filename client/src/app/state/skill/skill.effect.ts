b114aa72f2befaa050bbb82a331cc2e16e3da433
0918nobita <nobita.0918@gmail.com>
2019-01-02 17:06:33 +0900

    :zap: スキルの追加/削除処理を高速化した

--- client/src/app/state/skill/skill.effect.ts
+++ client/src/app/state/skill/skill.effect.ts
@@ -15,18 +15,17 @@ import {
   UpdateSkill
 } from '../skill/skill.action';
 import { concatMap, catchError, map } from 'rxjs/operators';
-import { AngularFireFunctions } from '@angular/fire/functions';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Skill } from '../../../app/models/skill';
 import { AuthEffects } from '../auth/auth.effect';
 import { pickOnce } from '../book/book.effect';
+import firebase from 'firebase/app';
 
 @Injectable()
 export class SkillEffects {
   constructor(
     private authEffects: AuthEffects,
     private actions$: Actions,
-    private afFunctions: AngularFireFunctions,
     private afFirestore: AngularFirestore
   ) {}
 
@@ -51,33 +50,50 @@ export class SkillEffects {
     ofType<CreateSkill>(SkillActionTypes.CreateSkill),
     concatMap(async action => {
       const uid = await pickOnce(this.authEffects.forAuthenticated);
-      await this.afFunctions
-        .httpsCallable('createSkill')({
-          isbn: action.isbn,
-          content: action.content,
-          uid
-        })
-        .toPromise();
-      console.log(`Created skill: ${action.content}`);
+      const { docs } = await pickOnce(
+        this.afFirestore
+          .collection('skills', ref =>
+            ref
+              .where('isbn', '==', action.isbn)
+              .where('content', '==', action.content)
+          )
+          .get()
+      );
+      if (docs.length !== 0) {
+        throw new Error('すでに同じスキルが追加されています');
+      }
+      await this.afFirestore.collection('skills').add({
+        uid,
+        isbn: action.isbn,
+        content: action.content,
+        created: firebase.firestore.Timestamp.fromDate(new Date())
+      });
       return new CreateSkillSuccess();
     }),
-    catchError(() => of(new CreateSkillFail()))
+    catchError(e => of(new CreateSkillFail(e)))
   );
 
   @Effect()
   deleteSkill: Observable<Action> = this.actions$.pipe(
     ofType<DeleteSkill>(SkillActionTypes.DeleteSkill),
     concatMap(async action => {
-      await this.afFunctions
-        .httpsCallable('deleteSkill')({
-          isbn: action.skill.isbn,
-          content: action.skill.content,
-          uid: action.skill.uid
-        })
-        .toPromise();
-      console.log(`Deleted skill: ${action.skill.content}`);
+      const { docs } = await pickOnce(
+        this.afFirestore
+          .collection('skills', ref =>
+            ref
+              .where('uid', '==', action.skill.uid)
+              .where('isbn', '==', action.skill.isbn)
+              .where('content', '==', action.skill.content)
+          )
+          .get()
+      );
+      if (docs.length === 0) throw new Error('スキル情報の取得に失敗しました');
+      await this.afFirestore
+        .collection('skills')
+        .doc(docs[0].id)
+        .delete();
       return new DeleteSkillSuccess();
     }),
-    catchError(() => of(new DeleteSkillFail()))
+    catchError(e => of(new DeleteSkillFail(e)))
   );
 }
